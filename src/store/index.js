53aee1c9bb5b92aed2d649dff9eb963b29c660a3
Danilo Ercoli <ercoli@gmail.com>
2018-07-18 17:47:39 +0200

    Load latest GB hash that has RichText with AztecRN "working"

--- src/store/index.js
+++ src/store/index.js
@@ -41,23 +41,19 @@ const initialMoreBlockHtml = `
 <!-- /wp:more -->
 `;
 
+const initialParagraphBlockHtml = '<!-- wp:paragraph --><p><b>Hello</b> World!</p><!-- /wp:paragraph -->';
+
+const initialParagraphBlockHtml2 = '<!-- wp:paragraph --><p>World!</p><!-- /wp:paragraph -->';
+
 const codeBlockInstance = parse( initialCodeBlockHtml )[ 0 ];
 const moreBlockInstance = parse( initialMoreBlockHtml )[ 0 ];
+const paragraphBlockInstance = parse( initialParagraphBlockHtml )[ 0 ];
+const paragraphBlockInstance2 = parse( initialParagraphBlockHtml2 )[ 0 ];
 
 const initialState: StateType = {
 	// TODO: get blocks list block state should be externalized (shared with Gutenberg at some point?).
 	// If not it should be created from a string parsing (commented HTML to json).
 	blocks: [
-		{
-			uid: '0',
-			name: 'aztec',
-			isValid: true,
-			attributes: {
-				content: 'This is text rendered <b>in Aztec!</b>',
-			},
-			innerBlocks: [],
-			focused: false,
-		},
 		{
 			uid: '1',
 			name: 'title',
@@ -68,47 +64,16 @@ const initialState: StateType = {
 			innerBlocks: [],
 			focused: false,
 		},
-		{
-			uid: '2',
-			name: 'paragraph',
-			isValid: true,
-			attributes: {
-				content:
-					'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempor tincidunt sapien, quis dictum orci sollicitudin quis. Proin sed elit id est pulvinar feugiat vitae eget dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-			},
-			innerBlocks: [],
-			focused: false,
-		},
-		{
-			uid: '3',
-			name: 'paragraph',
-			isValid: true,
-			attributes: {
-				content:
-					'書籍やウェブページや広告などのデザインのプロトタイプを制作したり顧客にプレゼンテーションしたりする際に、まだ正式な文章の出来上がっていないテキスト部分の書体（フォント）、タイポグラフィ、レイアウトなどといった視覚的なデザインを調整したりわかりやすく見せるために用いられる。',
-			},
-			innerBlocks: [],
-			focused: false,
-		},
+		{ ...paragraphBlockInstance, focused: false },
+		{ ...paragraphBlockInstance2, focused: false },
 		{ ...codeBlockInstance, focused: false },
 		{ ...moreBlockInstance, focused: false },
-		{
-			uid: '5',
-			name: 'paragraph',
-			isValid: true,
-			attributes: {
-				content:
-					'Лорем ипсум долор сит амет, адиписци трацтатос еа еум. Меа аудиам малуиссет те, хас меис либрис елеифенд ин. Нец ех тота деленит сусципит. Яуас порро инструцтиор но нец.',
-			},
-			innerBlocks: [],
-			focused: false,
-		},
 	],
 	refresh: false,
 };
 
 const devToolsEnhancer =
-	// ( 'development' === process.env.NODE_ENV && require( 'remote-redux-devtools' ).default ) ||
+	( 'development' === process.env.NODE_ENV && require( 'remote-redux-devtools' ).default ) ||
 	( () => {} );
 
 export function setupStore( state: StateType = initialState ) {
