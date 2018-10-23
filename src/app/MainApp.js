5e5fafbbf729acc6233915ad6f9270b23bd696b1
Marko Savic <marko.savic@automattic.com>
2018-10-23 18:08:41 +0200

    Implement Slot/Fill "portal" pattern in gutenberg mobile

--- src/app/MainApp.js
+++ src/app/MainApp.js
@@ -4,12 +4,13 @@
 import React from 'react';
 
 import BlockManager, { type BlockListType } from '../block-management/block-manager';
+import { SlotFillProvider } from '@wordpress/components';
 
 type PropsType = BlockListType;
 type StateType = {};
 
 export default class MainScreen extends React.Component<PropsType, StateType> {
 	render() {
-		return <BlockManager { ...this.props } />;
+		return (<SlotFillProvider><BlockManager { ...this.props } /></SlotFillProvider>);
 	}
 }
