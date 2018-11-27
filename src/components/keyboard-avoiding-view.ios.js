14f24e3db49012ba5e8fd29908b2fe5a202234f2
Marko Savic <savicmarko1985@gmail.com>
2018-11-27 19:38:41 +0100

    Small fix

--- src/components/keyboard-avoiding-view.ios.js
+++ src/components/keyboard-avoiding-view.ios.js
@@ -7,7 +7,7 @@ import React from 'react';
 import { KeyboardAvoidingView as IOSKeyboardAvoidingView, Dimensions } from 'react-native';
 
 const KeyboardAvoidingView = ( props ) => {
-	const { style, parentHeight } = props;
+	const { parentHeight } = props;
 	const { height: fullHeight } = Dimensions.get( 'window' );
 	const keyboardVerticalOffset = fullHeight - parentHeight;
 
