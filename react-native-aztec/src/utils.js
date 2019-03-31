a12ca56b13d1ba5a689bb6a5b13e7fbc7722f035
hotchemi <hot.chemistry21@gmail.com>
2019-04-01 06:23:25 +0900

    Add comment and tweak a way to compare string.

--- react-native-aztec/src/utils.js
+++ react-native-aztec/src/utils.js
@@ -1,7 +1,10 @@
 import { Platform } from 'react-native';
 
+// Basically React Native has a mechanism to scale font size automatically.
+// But GM is rendered on top of fragment of which retain flag is true.
+// Hence we deal with fontScale change manually on our side.
 export function getScaledFontSize( fontSize, fontScale ) {
-	if ( Platform.OS == 'ios' ) {
+	if ( Platform.OS === 'ios' ) {
 		return fontSize ? fontSize : undefined; // to not to affect to iOS
 	}
 	const baseFontSize = 18;
