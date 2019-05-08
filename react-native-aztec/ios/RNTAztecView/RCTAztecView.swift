c3fe3ca8332629d63e849d7bee698d39c1e21676
Sergio Estevao <sergioestevao@gmail.com>
2019-05-08 11:27:16 +0100

    Update Aztec version.

--- react-native-aztec/ios/RNTAztecView/RCTAztecView.swift
+++ react-native-aztec/ios/RNTAztecView/RCTAztecView.swift
@@ -183,7 +183,7 @@ class RCTAztecView: Aztec.TextView {
     }
 
     private func cleanHTML() -> String {
-        let html = getHTML(prettify: false)
+        let html = getHTML(prettify: false).replacingOccurrences(of: String(.paragraphSeparator), with: "")
         return html
     }
 
