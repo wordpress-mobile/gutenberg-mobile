80030aa5148d33695d423802b16fcf72fa2f19ed
Jorge Bernal <jbernal@gmail.com>
2019-06-18 12:45:16 +0200

    Haul, take 2, still not working

--- haul.config.js
+++ haul.config.js
@@ -6,5 +6,18 @@ import { createWebpackConfig } from 'haul';
 export default {
 	webpack: createWebpackConfig( ( { platform } ) => ( {
 		entry: `./index.js`,
+		rules: [ {
+			test: /\.scss$/,
+			use: [
+				'sass-loader', // compiles Sass to CSS, using Node Sass by default
+			],
+		} ],
+		plugins: [
+			[ 'module-resolver', {
+				alias: {
+					'^@wordpress/(.+)': 'gutenberg/packages/\\1',
+				},
+			} ],
+		],
 	} ) ),
 };
