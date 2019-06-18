72082943411965e1da55dcc190c9ddce2a6e051e
etoledom <etoledom@icloud.com>
2019-06-18 09:24:04 +0200

    Swift code clean up

--- react-native-plain-text/ios/RNPlainTextManager.m
+++ react-native-plain-text/ios/RNPlainTextManager.m
@@ -2,6 +2,6 @@
 
 @interface RCT_EXTERN_MODULE(RNPlainTextManager, NSObject)
 
-//RCT_EXPORT_VIEW_PROPERTY(onEnter, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onEnter, RCTBubblingEventBlock)
+
 @end
--- react-native-plain-text/ios/RNPlainTextManager.swift
+++ react-native-plain-text/ios/RNPlainTextManager.swift
@@ -5,8 +5,8 @@ public class RNPlainTextManager: RCTBaseTextInputViewManager {
     public override static func requiresMainQueueSetup() -> Bool {
         return true
     }
-    @objc
-    public override func view() -> UIView {
+
+    @objc public override func view() -> UIView {
         return RNBackedPlainTextView(bridge: self.bridge)
     }
 }
