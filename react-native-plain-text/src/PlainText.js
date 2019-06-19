6aef48bb31218b4175a6d88d9d20d74510939fe2
Danilo Ercoli <ercoli@gmail.com>
2019-06-19 14:54:14 +0200

    Introduce two render methods, one for each family of devices (Android | iOS)

--- react-native-plain-text/src/PlainText.js
+++ react-native-plain-text/src/PlainText.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { requireNativeComponent, TextInput, StyleSheet } from 'react-native';
+import { requireNativeComponent, TextInput, StyleSheet, Platform, UIManager, TouchableWithoutFeedback } from 'react-native';
 
 const emptyFunctionThatReturnsTrue = () => true;
 
@@ -18,6 +18,15 @@ class PlainText extends TextInput {
 	}
 
 	render() {
+		let textInput;
+		if (Platform.OS === 'ios') {
+			return this._wpRenderIOS()
+		} else if (Platform.OS === 'android') {
+			return this._wpRenderAndroid();
+		}
+	  }
+
+	_wpRenderIOS() {
 		const props = Object.assign( {}, this.props );
 		props.style = [ this.props.style ];
 
@@ -48,6 +57,71 @@ class PlainText extends TextInput {
 			/>
 		);
 	}
+
+	_wpRenderAndroid() {
+		const props = Object.assign({}, this.props);
+		props.style = [this.props.style];
+		props.autoCapitalize = UIManager.getViewManagerConfig(
+		  'AndroidTextInput',
+		).Constants.AutoCapitalizationType[props.autoCapitalize || 'sentences'];
+		/* $FlowFixMe(>=0.53.0 site=react_native_fb,react_native_oss) This comment
+		 * suppresses an error when upgrading Flow's support for React. To see the
+		 * error delete this comment and run Flow. */
+		let children = this.props.children;
+		let childCount = 0;
+		React.Children.forEach(children, () => ++childCount);
+		/*
+		invariant( 
+			!(this.props.value && childCount),
+			'Cannot specify both value and children.',
+			);
+		*/
+		if (childCount > 1) {
+		  children = <Text>{children}</Text>;
+		}
+	
+		if (props.selection && props.selection.end == null) {
+		  props.selection = {
+			start: props.selection.start,
+			end: props.selection.start,
+		  };
+		}
+
+		const textContainer = (
+		  <RNPlainText
+			ref={ ( ref ) => {
+				this._editor = ref;
+				this._setNativeRef( ref );
+			} }
+			{ ...props }
+			mostRecentEventCount={0}
+			onFocus={this._onFocus}
+			onBlur={this._onBlur}
+			onChange={this._onChange}
+			onSelectionChange={this._onSelectionChange}
+			onTextInput={this._onTextInput}
+			text={this._getText()}
+			children={children}
+			disableFullscreenUI={this.props.disableFullscreenUI}
+			textBreakStrategy={this.props.textBreakStrategy}
+			onScroll={this._onScroll}
+		  />
+		);
+	
+		return (
+		  <TouchableWithoutFeedback
+			onLayout={props.onLayout}
+			onPress={this._onPress}
+			accessible={this.props.accessible}
+			accessibilityLabel={this.props.accessibilityLabel}
+			accessibilityRole={this.props.accessibilityRole}
+			accessibilityStates={this.props.accessibilityStates}
+			nativeID={this.props.nativeID}
+			testID={this.props.testID}>
+			{textContainer}
+		  </TouchableWithoutFeedback>
+		);
+	  }
 }
 
 const RNPlainText = requireNativeComponent( 'RNPlainText', PlainText );
