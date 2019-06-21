307780e1e4bc3e52d37af25b8605dbceef3608af
Pinar Olguc <pinarolguc@gmail.com>
2019-06-21 17:37:00 +0300

    Update `rn-cli.config.js` to load from an outer folder

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -8,10 +8,22 @@ const blacklistElements = blacklist( [
 	new RegExp( path.basename( __dirname ) + '/gutenberg/gutenberg-mobile/.*' ),
 	new RegExp( path.basename( __dirname ) + '/react-native-aztec-old-submodule/.*' ),
 ] );
+
+function extend(obj, src) {
+    Object.keys(src).forEach(function(key) { obj[key] = src[key]; });
+    return obj;
+}
+
 const enm = require( './extra-node-modules.config.js' );
 
 module.exports = {
-	extraNodeModules: enm,
+	watchFolders:[ '/Users/pinarolguc/Development/mobile-gutenberg-plugin-starter-test/plugins/gutenberg-plugin-hello-world' ],
+	extraNodeModules: extend (
+    {
+      '@wordpress-mobile/gutenberg-plugin-hello-world': '/Users/pinarolguc/Development/mobile-gutenberg-plugin-starter-test/plugins/gutenberg-plugin-hello-world'
+  	},
+    enm
+  ),
 	resolver: {
 		blacklistRE: blacklistElements,
 		sourceExts: [ 'js', 'json', 'scss', 'sass' ],
