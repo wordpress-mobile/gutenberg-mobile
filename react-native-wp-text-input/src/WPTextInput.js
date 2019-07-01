4c3f5fa9947d9fd2146142b9a71c9d71373dad7d
etoledom <etoledom@icloud.com>
2019-07-01 10:17:56 +0200

    Fix lint issues

--- react-native-wp-text-input/src/WPTextInput.js
+++ react-native-wp-text-input/src/WPTextInput.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { requireNativeComponent, TextInput, StyleSheet, Platform, UIManager, TouchableWithoutFeedback } from 'react-native';
+import { requireNativeComponent, TextInput, Text, Platform, UIManager, TouchableWithoutFeedback } from 'react-native';
 const NativeWPTextInput = requireNativeComponent( 'WPTextInput' );
 
 const emptyFunctionThatReturnsTrue = () => true;
@@ -19,13 +19,12 @@ export default class WPTextInput extends TextInput {
 	}
 
 	render() {
-		let textInput;
-		if (Platform.OS === 'ios') {
-			return this._wpRenderIOS()
-		} else if (Platform.OS === 'android') {
+		if ( Platform.OS === 'ios' ) {
+			return this._wpRenderIOS();
+		} else if ( Platform.OS === 'android' ) {
 			return this._wpRenderAndroid();
 		}
-	  }
+	}
 
 	_wpRenderIOS() {
 		const props = Object.assign( {}, this.props );
@@ -60,67 +59,67 @@ export default class WPTextInput extends TextInput {
 	}
 
 	_wpRenderAndroid() {
-		const props = Object.assign({}, this.props);
-		props.style = [this.props.style];
+		const props = Object.assign( {}, this.props );
+		props.style = [ this.props.style ];
 		props.autoCapitalize = UIManager.getViewManagerConfig(
-		  'AndroidTextInput',
-		).Constants.AutoCapitalizationType[props.autoCapitalize || 'sentences'];
+			'AndroidTextInput',
+		).Constants.AutoCapitalizationType[ props.autoCapitalize || 'sentences' ];
 		/* $FlowFixMe(>=0.53.0 site=react_native_fb,react_native_oss) This comment
-		 * suppresses an error when upgrading Flow's support for React. To see the
-		 * error delete this comment and run Flow. */
+		* suppresses an error when upgrading Flow's support for React. To see the
+		* error delete this comment and run Flow. */
 		let children = this.props.children;
 		let childCount = 0;
-		React.Children.forEach(children, () => ++childCount);
+		React.Children.forEach( children, () => ++childCount );
 		/*
-		invariant( 
+		invariant(
 			!(this.props.value && childCount),
 			'Cannot specify both value and children.',
 			);
 		*/
-		if (childCount > 1) {
-		  children = <Text>{children}</Text>;
+		if ( childCount > 1 ) {
+			children = <Text>{ children }</Text>;
 		}
-	
-		if (props.selection && props.selection.end == null) {
-		  props.selection = {
-			start: props.selection.start,
-			end: props.selection.start,
-		  };
+
+		if ( props.selection && props.selection.end === null ) {
+			props.selection = {
+				start: props.selection.start,
+				end: props.selection.start,
+			};
 		}
 
 		const textContainer = (
-		  <NativeWPTextInput
-			ref={ ( ref ) => {
-				this._editor = ref;
-				this._setNativeRef( ref );
-			} }
-			{ ...props }
-			mostRecentEventCount={0}
-			onFocus={this._onFocus}
-			onBlur={this._onBlur}
-			onChange={this._onChange}
-			onSelectionChange={this._onSelectionChange}
-			onTextInput={this._onTextInput}
-			text={this._getText()}
-			children={children}
-			disableFullscreenUI={this.props.disableFullscreenUI}
-			textBreakStrategy={this.props.textBreakStrategy}
-			onScroll={this._onScroll}
-		  />
+			<NativeWPTextInput
+				ref={ ( ref ) => {
+					this._editor = ref;
+					this._setNativeRef( ref );
+				} }
+				{ ...props }
+				mostRecentEventCount={ 0 }
+				onFocus={ this._onFocus }
+				onBlur={ this._onBlur }
+				onChange={ this._onChange }
+				onSelectionChange={ this._onSelectionChange }
+				onTextInput={ this._onTextInput }
+				text={ this._getText() }
+				children={ children }
+				disableFullscreenUI={ this.props.disableFullscreenUI }
+				textBreakStrategy={ this.props.textBreakStrategy }
+				onScroll={ this._onScroll }
+			/>
 		);
-	
+
 		return (
-		  <TouchableWithoutFeedback
-			onLayout={props.onLayout}
-			onPress={this._onPress}
-			accessible={this.props.accessible}
-			accessibilityLabel={this.props.accessibilityLabel}
-			accessibilityRole={this.props.accessibilityRole}
-			accessibilityStates={this.props.accessibilityStates}
-			nativeID={this.props.nativeID}
-			testID={this.props.testID}>
-			{textContainer}
-		  </TouchableWithoutFeedback>
+			<TouchableWithoutFeedback
+				onLayout={ props.onLayout }
+				onPress={ this._onPress }
+				accessible={ this.props.accessible }
+				accessibilityLabel={ this.props.accessibilityLabel }
+				accessibilityRole={ this.props.accessibilityRole }
+				accessibilityStates={ this.props.accessibilityStates }
+				nativeID={ this.props.nativeID }
+				testID={ this.props.testID }>
+				{ textContainer }
+			</TouchableWithoutFeedback>
 		);
-	  }
+	}
 }
