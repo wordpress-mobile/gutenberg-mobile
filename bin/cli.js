aac908cc1ce6751b2efa2547f054477202ed94e9
Jorge Bernal <jorge@automattic.com>
2019-07-17 17:45:48 +0200

    Add haul configuration with alias and css loading

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,11 @@
 const path = require( 'path' );
 
 const projectRoot = path.resolve( __dirname, '..' );
-const haulCli = path.resolve( projectRoot, 'node_modules/.bin/haul' );
-const metroCli = path.resolve( projectRoot, 'node_modules/.bin/react-native' );
 
 if ( process.env.PACKAGER === 'haul' ) {
+	const haulCli = path.resolve( projectRoot, 'node_modules/.bin/haul' );
 	require( haulCli );
 } else {
-	require( metroCli );
+	const cli = require( '@react-native-community/cli' );
+	cli.run();
 }
---
+++ haul.config.js
@@ -0,0 +1,69 @@
+/**
+ * External dependencies
+ */
+import { makeConfig, withPolyfills } from '@haul-bundler/preset-0.60';
+import path from 'path';
+
+const gutenbergPackages = ( () => {
+	const dependencies = require( './gutenberg/package' ).dependencies;
+	return Object.keys( dependencies ).reduce( ( memo, module ) => {
+		memo[ module ] = path.resolve( dependencies[ module ].replace( /^file:/, 'gutenberg/' ) );
+		return memo;
+	}, {} );
+} )();
+
+export default makeConfig( {
+	bundles: {
+		index: {
+			entry: withPolyfills( './index.js' ),
+			transform( { bundleName, env, runtime, config } ) {
+				const { platform } = env;
+
+				config.module.rules = [
+					...config.module.rules,
+					{
+						test: /\.scss$/,
+						use: [
+							{
+								loader: path.resolve( 'webpack-react-native-sass-loader.js' ),
+								options: {
+									platform,
+								},
+							},
+							{
+								loader: 'sass-loader',
+								options: {
+									includePaths: [
+										path.resolve( 'src' ),
+										path.resolve( 'gutenberg/assets/stylesheets' ),
+									],
+								},
+							},
+							{
+								loader: path.resolve( 'sass-auto-imports.js' ),
+								options: {
+									files: [
+										'_colors.scss',
+										'_breakpoints.scss',
+										'_variables.scss',
+										platform === 'android' ? '_native.android.scss' : '_native.ios.scss',
+										'_mixins.scss',
+										'_animations.scss',
+										'_z-index.scss',
+									],
+								},
+							},
+						],
+					},
+				];
+
+				config.resolve.alias = {
+					...config.resolve.alias,
+					...gutenbergPackages,
+					'react-native-aztec': path.resolve( 'react-native-aztec' ),
+					'react-native-gutenberg-bridge': path.resolve( 'react-native-gutenberg-bridge' ),
+				};
+			},
+		},
+	},
+} );
