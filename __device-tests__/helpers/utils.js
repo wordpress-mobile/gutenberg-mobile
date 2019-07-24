0e95f9ad4a8b7464984f3c550e7037b51d141d6e
Javon <javondavis@users.noreply.github.com>
2019-07-24 14:52:06 -0500

    fix linter complaint

--- __device-tests__/helpers/utils.js
+++ __device-tests__/helpers/utils.js
@@ -144,7 +144,7 @@ const setupDriver = async () => {
 		desiredCaps = _.clone( ios12 );
 		if ( isLocalEnvironment() ) {
 			const simulators = await getIOSDevices();
-			const { platformVersion, udid } = simulators[ process.env.JEST_WORKER_ID - 1 ]; // Pick a unique simulator for this run
+			const { platformVersion, udid } = simulators[ parseInt(process.env.JEST_WORKER_ID) - 1 ]; // Pick a unique simulator for this run
 			desiredCaps.platformVersion = platformVersion;
 			desiredCaps.udid = udid;
 			desiredCaps.app = path.resolve( localIOSAppPath );
