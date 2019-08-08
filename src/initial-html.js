bb8af106ca16876ab89eed84e3dbee1785e23750
Jorge Bernal <jorge@automattic.com>
2019-08-08 11:36:41 +0200

    Fix serialization errors

--- src/initial-html.js
+++ src/initial-html.js
@@ -4,7 +4,7 @@
  */
 
 export default `
-<!-- wp:gallery {"ids":[681,682],"columns":6} -->
-<ul class="wp-block-gallery columns-6 is-cropped"><li class="blocks-gallery-item"><figure><img alt="" data-id="681" data-link="https://sandbox.koke.me/?attachment_id=681" class="wp-image-681" tagname="img" src="https://sandbox.koke.me/wp-content/uploads/2019/07/fullsizeoutput_6d32-1024x1024.jpeg"/></figure></li><li class="blocks-gallery-item"><figure><img alt="" data-id="682" data-link="https://sandbox.koke.me/?attachment_id=682" class="wp-image-682" tagname="img" src="https://sandbox.koke.me/wp-content/uploads/2019/07/fullsizeoutput_6d0a-1024x1022.jpeg"/></figure></li></ul>
+<!-- wp:gallery {"ids":[685,687,684],"columns":2} -->
+<ul class="wp-block-gallery columns-2 is-cropped"><li class="blocks-gallery-item"><figure><img src="https://sandbox.koke.me/wp-content/uploads/2019/07/E4D6D6CD-7C80-40BE-9A25-886D47B96791-1024x1024.jpg" alt="" data-id="685" data-link="https://sandbox.koke.me/?attachment_id=685" class="wp-image-685"/></figure></li><li class="blocks-gallery-item"><figure><img src="https://sandbox.koke.me/wp-content/uploads/2019/07/UcdzcI53QTenNzbk5Lx0PQ-e1563887552937-768x1024.jpg" alt="" data-id="687" data-link="https://sandbox.koke.me/?attachment_id=687" class="wp-image-687"/></figure></li><li class="blocks-gallery-item"><figure><img src="https://sandbox.koke.me/wp-content/uploads/2019/07/pKuQ70wSK6IDok41tcQ-1024x768.jpg" alt="" data-id="684" data-link="https://sandbox.koke.me/?attachment_id=684" class="wp-image-684"/></figure></li></ul>
 <!-- /wp:gallery -->
 `;
