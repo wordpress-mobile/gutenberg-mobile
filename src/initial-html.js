86b12b44bb1cd5a85d62139fa09ef8be98c806bb
lukewalczak <lukasz.walczak.pwr@gmail.com>
2019-11-06 10:47:23 +0100

    Add ref to gutenberg repo

--- src/initial-html.js
+++ src/initial-html.js
@@ -8,6 +8,157 @@ export default `
 <figure class="wp-block-image"><img alt=""/></figure>
 <!-- /wp:image -->
 
+<!-- wp:heading -->
+<h2>Taller text</h2>
+<!-- /wp:heading -->
+<!-- wp:media-text {"mediaId":2213,"mediaType":"image","verticalAlignment":"top"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"><img src="https://breakmeplease.files.wordpress.com/2019/10/kitty-cat-kitten-pet-45201.jpeg?w=625" alt="" class="wp-image-2213"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph -->
+<p>One paragraph</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Two paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Three paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Four paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Five paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Six paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Seven paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Eight paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Nine paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Ten paragraphs</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+<!-- wp:media-text {"mediaId":2213,"mediaType":"image","verticalAlignment":"center"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-center"><figure class="wp-block-media-text__media"><img src="https://breakmeplease.files.wordpress.com/2019/10/kitty-cat-kitten-pet-45201.jpeg?w=625" alt="" class="wp-image-2213"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph -->
+<p>One paragraph</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Two paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Three paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Four paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Five paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Six paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Seven paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Eight paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Nine paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Ten paragraphs</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+<!-- wp:media-text {"mediaId":2213,"mediaType":"image","verticalAlignment":"bottom"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-bottom"><figure class="wp-block-media-text__media"><img src="https://breakmeplease.files.wordpress.com/2019/10/kitty-cat-kitten-pet-45201.jpeg?w=625" alt="" class="wp-image-2213"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph -->
+<p>One paragraph</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Two paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Three paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Four paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Five paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Six paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Seven paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Eight paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Nine paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Ten paragraphs</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+<!-- wp:heading -->
+<h2>Taller image</h2>
+<!-- /wp:heading -->
+<!-- wp:media-text {"mediaId":2213,"mediaType":"image","verticalAlignment":"top"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-top"><figure class="wp-block-media-text__media"><img src="https://breakmeplease.files.wordpress.com/2019/10/kitty-cat-kitten-pet-45201.jpeg?w=625" alt="" class="wp-image-2213"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
+<p class="has-large-font-size">A cute kitten</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+<!-- wp:media-text {"mediaId":2213,"mediaType":"image"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"><img src="https://breakmeplease.files.wordpress.com/2019/10/kitty-cat-kitten-pet-45201.jpeg?w=625" alt="" class="wp-image-2213"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
+<p class="has-large-font-size">A cute kitten</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+<!-- wp:media-text {"mediaId":2213,"mediaType":"image","verticalAlignment":"bottom"} -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile is-vertically-aligned-bottom"><figure class="wp-block-media-text__media"><img src="https://breakmeplease.files.wordpress.com/2019/10/kitty-cat-kitten-pet-45201.jpeg?w=625" alt="" class="wp-image-2213"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
+<p class="has-large-font-size">A cute kitten</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+<!-- wp:heading -->
+<h2>Placeholder</h2>
+<!-- /wp:heading -->
+<!-- wp:media-text -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
+<p class="has-large-font-size"></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+<!-- wp:media-text -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
+<p class="has-large-font-size">One paragraph</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+<!-- wp:media-text -->
+<div class="wp-block-media-text alignwide is-stacked-on-mobile"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:paragraph -->
+<p>One paragraph</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Two paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Three paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Four paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Five paragraphs</p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>Six paragraphs</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+
 <!-- wp:media-text -->
 <div class="wp-block-media-text is-stacked-on-mobile"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…","fontSize":"large"} -->
 <p class="has-large-font-size"></p>
