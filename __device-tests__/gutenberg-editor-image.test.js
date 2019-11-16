7e84a26f8c4c5f319dffedfe1747714fe5c8374a
RachelleT <rache_trotman@hotmail.com>
2019-11-15 22:42:35 -0500

    Updating code after reset

--- __device-tests__/gutenberg-editor-image.test.js
+++ __device-tests__/gutenberg-editor-image.test.js
@@ -39,6 +39,10 @@ describe( 'Gutenberg Editor Image Block tests', () => {
 		editorPage = new EditorPage( driver );
 	} );
 
+	afterEach( async () => {
+        await editorPage.removeBlocks();
+    } );
+
 	it( 'should be able to see visual editor', async () => {
 		await expect( editorPage.getBlockList() ).resolves.toBe( true );
 	} );
