29bf4db31299822ae881e2b2c3be59a093bc617b
Matthew Kevins <mmkevins@yahoo.com>
2019-12-06 12:05:33 +1000

    Restore demo content

--- src/initial-html.js
+++ src/initial-html.js
@@ -4,9 +4,6 @@
  */
 
 export default `
-<!-- wp:gallery {"ids":[67,68,69,70,71]} -->
-<ul class="wp-block-gallery columns-3 is-cropped"><li class="blocks-gallery-item"><figure><img src="https://wuweive.files.wordpress.com/2019/09/sninskycc81_kamencc8c_v_zime_003.jpg?w=1024" alt="" data-id="67" data-link="https://wuweive.wordpress.com/sninskycc81_kamencc8c_v_zime_003/" class="wp-image-67"/></figure></li><li class="blocks-gallery-item"><figure><img src="https://wuweive.files.wordpress.com/2019/09/sninskycc81_kamencc8c_v_zime_029.jpg?w=1024" alt="" data-id="68" data-link="https://wuweive.wordpress.com/sninskycc81_kamencc8c_v_zime_029/" class="wp-image-68"/></figure></li><li class="blocks-gallery-item"><figure><img src="https://wuweive.files.wordpress.com/2019/09/sossusvlei_sunrise_hdr.jpg?w=686" alt="" data-id="69" data-link="https://wuweive.wordpress.com/sossusvlei_sunrise_hdr/" class="wp-image-69"/></figure></li><li class="blocks-gallery-item"><figure><img src="https://wuweive.files.wordpress.com/2019/09/virmalised_18.03.15_-_aurora_borealis_18.03.15_4.jpg?w=1024" alt="" data-id="70" data-link="https://wuweive.wordpress.com/virmalised_18-03-15_-_aurora_borealis_18-03-15_4/" class="wp-image-70"/></figure></li><li class="blocks-gallery-item"><figure><img src="https://wuweive.files.wordpress.com/2019/09/west_thumb_geysir_basin_2017_08.jpg?w=1024" alt="" data-id="71" data-link="https://wuweive.wordpress.com/west_thumb_geysir_basin_2017_08/" class="wp-image-71"/></figure></li></ul>
-<!-- /wp:gallery -->
 <!-- wp:image -->
 <figure class="wp-block-image"><img alt=""/></figure>
 <!-- /wp:image -->
@@ -215,4 +212,7 @@ else:
 <h2 class="has-text-align-left">Dig in deeper</h2>
 <!-- /wp:heading -->
 
+<!-- wp:list -->
+<ul><li><a href="https://make.wordpress.org/core/2017/01/17/editor-technical-overview">Gutenberg Editor Technical Overview</a></li><li><a href="https://wordpress.org/gutenberg/handbook/reference/design-principles/">Gutenberg Design Principles</a></li><li><a href="https://make.wordpress.org/core/tag/gutenberg/">Development updates on make.wordpress.org</a></li><li><a href="https://wordpress.tv/?s=gutenberg">WordPress.tv Talks about Gutenberg</a></li><li><a href="https://wordpress.org/gutenberg/handbook/reference/faq/">FAQs</a></li></ul>
+<!-- /wp:list -->
 `;
