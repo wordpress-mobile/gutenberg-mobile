00503480930bb8c0ec6615494662f3fcb3a8e83e
Klymentiy haykov <forsver@gmail.com>
2020-03-29 15:07:05 -0700

    Accept multiple headers through OnAuthHeaderRequestedListener

--- react-native-gutenberg-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/WPAndroidGlueCode.java
+++ react-native-gutenberg-bridge/android/src/main/java/org/wordpress/mobile/WPAndroidGlue/WPAndroidGlueCode.java
@@ -146,7 +146,7 @@ public class WPAndroidGlueCode {
     }
 
     public interface OnAuthHeaderRequestedListener {
-        String onAuthHeaderRequested(String url);
+        Map<String, String> onAuthHeaderRequested(String url);
     }
 
     public interface OnEditorAutosaveListener {
