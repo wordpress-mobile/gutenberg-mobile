e41c28b7202222b82b524121628e9699f1971c9a
Tugdual de Kerviler <dekervit@gmail.com>
2020-04-24 12:31:33 +0200

    Simply export @wordpress/react-native-editor metro config file as our own

---
+++ babel.config.js
@@ -0,0 +1,1 @@
+module.exports = require( './gutenberg/packages/react-native-editor/babel.config.js' );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-/**
- * WordPress dependencies
- */
-import { gutenbergSetup, registerApp } from '@wordpress/react-native-editor';
-
-gutenbergSetup();
 
 const wpHooks = require( '@wordpress/hooks' );
 
@@ -11,4 +5,5 @@ wpHooks.addAction( 'native.render', 'core/react-native-editor', ( props ) => {
 	console.log( 'rendered from gutenberg-mobile', props );
 } );
 
-registerApp();
+require( '@wordpress/react-native-editor' );
+
