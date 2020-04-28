1f3a66df060cd0dc0cf0245a745a320cf1761574
Tugdual de Kerviler <dekervit@gmail.com>
2020-04-28 17:52:11 +0200

    Experiment using @carimus/metro-symlinked-deps to support symlinks

--- metro.config.js
+++ metro.config.js
@@ -1,1 +1,20 @@
-module.exports = require( './gutenberg/packages/react-native-editor/metro.config.js' );
+const path = require( 'path' );
+const {
+	applyConfigForLinkedDependencies,
+} = require( '@carimus/metro-symlinked-deps' );
+
+const additionalWatchFolders = [
+	'@automattic/full-site-editing',
+].map( ( dependency ) => path.dirname( require.resolve( `${ dependency }/package.json` ) ) );
+
+module.exports = applyConfigForLinkedDependencies(
+	require( './gutenberg/packages/react-native-editor/metro.config.js' ),
+	{
+		projectRoot: path.resolve( __dirname ),
+		blacklistDirectories: [
+			'gutenberg/node_modules',
+		],
+		additionalWatchFolders,
+		resolveNodeModulesAtRoot: true,
+	}
+);
--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import setupJetpackEditor from './jetpack-editor-setup';
 addAction( 'native.render', 'core/react-native-editor', ( props ) => {
 	console.log( 'rendered from gutenberg-mobile', props );
 	setupJetpackEditor( props.jetpackState || { blogId: 1, isJetpackActive: true } );
+	require( '@automattic/full-site-editing/full-site-editing-plugin/posts-list-block' );
 } );
 
 require( '@wordpress/react-native-editor' );
