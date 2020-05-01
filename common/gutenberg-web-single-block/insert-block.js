20bc893af94e04af0cea6c2e8d8b723ef147a8c6
eToledo <etoledom@icloud.com>
2020-05-01 20:04:10 +0200

    Change strategy to insert block. In this way the `isDirty` flag is false if the user has no made any change. There are also no undo levels without user changes.

--- common/gutenberg-web-single-block/insert-block.js
+++ common/gutenberg-web-single-block/insert-block.js
@@ -1,9 +1,16 @@
 window.setTimeout( () => {
 	const blockHTML = `%@`;
-	const blocks = window.wp.blocks.parse( blockHTML );
-	const blockEditor = window.wp.data.dispatch( 'core/block-editor' ) ||
-        window.wp.data.dispatch( 'core/editor' ); // For WP v5.0 and v5.1
-	blockEditor.resetBlocks( blocks );
-	blockEditor.selectBlock( blocks[ 0 ].clientId );
-	window.webkit.messageHandlers.log.postMessage( 'Block inserted' );
+
+	// Setup the editor with the inserted block
+	const post = window.wp.data.select( 'core/editor' ).getCurrentPost();
+	window.wp.data.dispatch( 'core/editor' ).setupEditor( post, { content: blockHTML } );
+
+	// Select the first block
+	const blockEditorSelect = window.wp.data.select( 'core/block-editor' ) ||
+        window.wp.data.select( 'core/editor' ); // For WP v5.0 and v5.1
+	const blockEditorDispatch = window.wp.data.dispatch( 'core/block-editor' ) ||
+		window.wp.data.dispatch( 'core/editor' ); // For WP v5.0 and v5.1
+
+	const clientId = blockEditorSelect.getBlocks()[ 0 ].clientId;
+	blockEditorDispatch.selectBlock( clientId );
 }, 0 );
--- common/gutenberg-web-single-block/retrieve-html-content.js
+++ common/gutenberg-web-single-block/retrieve-html-content.js
@@ -6,22 +6,3 @@ window.getHTMLPostContent = () => {
 	window.webkit.messageHandlers.htmlPostContent.postMessage( HTML );
 };
 
-// Prevent posting autosaves to remote
-( function( open ) {
-	XMLHttpRequest.prototype.open = function( arg1, arg2 ) {
-		this.URL = arg2;
-		this.method = arg1;
-		open.apply( this, arguments );
-	};
-}( XMLHttpRequest.prototype.open ) );
-
-( function( send ) {
-	// eslint-disable-next-line no-unused-vars
-	XMLHttpRequest.prototype.send = function( arg1 ) {
-		if ( this.URL.includes( '/autosaves' ) && this.method === 'POST' ) {
-			this.abort();
-			return;
-		}
-		send.apply( this, arguments );
-	};
-}( XMLHttpRequest.prototype.send ) );
