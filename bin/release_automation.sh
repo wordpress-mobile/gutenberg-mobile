80f7cb7669f2fa04669429b3bf00d9e93fdbf229
Sergio Estevao <sergioestevao@gmail.com>
2020-06-17 21:54:07 +0100

    Update script to update PR template with version number.

--- bin/release_automation.sh
+++ bin/release_automation.sh
@@ -9,12 +9,16 @@ git switch -c release/$VERSION_NUMBER
 # Set version number in package.json
 yarn json -I -f package.json -e "this.version='$VERSION_NUMBER'"
 # Update the bundles
-#yarn bundle
+yarn bundle
 # Make sure podfile is updated
 pod install --project-directory=./ios/
 # Generate updated podspecs
 ./bin/generate-podspecs.sh
 # Commit changes
 git commit -a -m "Prepare Release $VERSION_NUMBER"
+# Read PR template
+PR_TEMPLATE=$(cat .github/PULL_REQUEST_TEMPLATE/release_pull_request.md)
+# Replace version number
+PR_BODY=${PR_TEMPLATE//v1.XX.Y/$VERSION_NUMBER}
 # Create PR in GitHub
-gh pr create -t "Release 1.31.0" -b "$(cat .github/PULL_REQUEST_TEMPLATE/release_pull_request.md)" -B master -l "release-process"
+gh pr create -t "Release $VERSION_NUMBER" -b "$PR_BODY" -B master -l "release-process"
