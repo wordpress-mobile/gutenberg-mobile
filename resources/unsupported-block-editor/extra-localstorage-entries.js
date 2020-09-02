4f1f501c8851ee96c9019719d982ed930f233cdb
eToledo <etoledom@icloud.com>
2020-09-02 16:32:45 +0200

    Fix lint issues

--- resources/unsupported-block-editor/extra-localstorage-entries.js
+++ resources/unsupported-block-editor/extra-localstorage-entries.js
@@ -1,13 +1,13 @@
 const storageKey = 'WP_DATA_USER_%@';
-const storage = localStorage.getItem(storageKey);
+const storage = localStorage.getItem( storageKey );
 
 if ( storage ) {
-	let parsed = JSON.parse(storage);
-	parsed['automattic/nux'] = {
-		"isNuxEnabled": false
+	const parsed = JSON.parse( storage );
+	parsed[ 'automattic/nux' ] = {
+		isNuxEnabled: false,
 	};
-	const newStorage = JSON.stringify(parsed);
-	localStorage.setItem(storageKey, newStorage);
+	const newStorage = JSON.stringify( parsed );
+	localStorage.setItem( storageKey, newStorage );
 }
 // We need to return a string or null, otherwise executing this script will error.
 // eslint-disable-next-line no-unused-expressions
