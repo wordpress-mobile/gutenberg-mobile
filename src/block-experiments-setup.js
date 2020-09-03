f322e4976cedc05f3f25189739810e3d9c59864a
Enej Bajgoric <enej.bajgoric@automattic.com>
2020-09-03 16:01:41 -0700

    Setup the block experiments, layout grid block and include it the editor

---
+++ src/block-experiments-setup.js
@@ -0,0 +1,5 @@
+// This file sets up the jetpack/layout-grid block that currently lives in block-experiments/blocks/layout-grid
+
+import { registerBlock } from '../block-experiments/blocks/layout-grid/src';
+
+registerBlock();
