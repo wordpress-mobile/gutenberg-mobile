c8abb06511c282a7031f546f61f4b2dc307f9b37
Wendy Chen <tilphousia@gmail.com>
2020-09-29 11:50:26 -0500

    WC: Fixing lint issues.

--- __device-tests__/gutenberg-editor-contact-info.test.js
+++ __device-tests__/gutenberg-editor-contact-info.test.js
@@ -14,6 +14,8 @@ import wd from 'wd';
 
 jest.setTimeout( 1000000 );
 
+// Disabling eslint here because these tests need to be disabled until Contact Info block is ungated from __DEV__.
+// eslint-disable-next-line jest/no-disabled-tests
 describe.skip( 'Gutenberg Editor Contact Info Block tests', () => {
 	let driver;
 	let editorPage;
@@ -32,34 +34,36 @@ describe.skip( 'Gutenberg Editor Contact Info Block tests', () => {
 		jasmine.getEnv().addReporter( reporter );
 	}
 
-	async function toggleSettings( driver, editorPage, block ) {
+	async function toggleSettings( drv, ePage, bk ) {
 		const buttonElementName = isAndroid()
 			? '//*'
 			: '//XCUIElementTypeButton';
-		const blockActionsMenuButtonLocator = `${ buttonElementName }[contains(@${ editorPage.accessibilityIdXPathAttrib }, "Open Settings")]`;
+		const blockActionsMenuButtonLocator = `${ buttonElementName }[contains(@${ ePage.accessibilityIdXPathAttrib }, "Open Settings")]`;
 
 		if ( isAndroid() ) {
-			let checkList = await driver.elementsByXPath(
+			let checkList = await drv.elementsByXPath(
 				blockActionsMenuButtonLocator
 			);
 			while ( checkList.length === 0 ) {
-				await swipeUp( driver, block ); // Swipe up to show remove icon at the bottom
-				checkList = await driver.elementsByXPath(
+				await swipeUp( drv, bk ); // Swipe up to show remove icon at the bottom
+				checkList = await drv.elementsByXPath(
 					blockActionsMenuButtonLocator
 				);
 			}
 		}
 
-		const blockActionsMenuButton = await driver.elementByXPath(
+		const blockActionsMenuButton = await drv.elementByXPath(
 			blockActionsMenuButtonLocator
 		);
 		await blockActionsMenuButton.click();
 	}
 
-	async function findElement( driver, editorPage, name, iosType ) {
-		const elementName = isAndroid() ? '//*' : `//XCUIElementType${iosType}`;
-		const blockLocator = `${ elementName }[contains(@${ editorPage.accessibilityIdXPathAttrib }, "${ name }")]`;
-		const elements = await driver.elementsByXPath( blockLocator );
+	async function findElement( drv, ePage, name, iosType ) {
+		const elementName = isAndroid()
+			? '//*'
+			: `//XCUIElementType${ iosType }`;
+		const blockLocator = `${ elementName }[contains(@${ ePage.accessibilityIdXPathAttrib }, "${ name }")]`;
+		const elements = await drv.elementsByXPath( blockLocator );
 		return elements[ 0 ];
 	}
 
@@ -92,7 +96,9 @@ describe.skip( 'Gutenberg Editor Contact Info Block tests', () => {
 	} );
 
 	it( 'should show keyboard when selecting email', async () => {
-		const emailBlock = await editorPage.getBlockAtPosition( 'Email Address' );
+		const emailBlock = await editorPage.getBlockAtPosition(
+			'Email Address'
+		);
 		emailBlock.click();
 
 		const keyboardShown = await driver.isKeyboardShown();
@@ -107,7 +113,10 @@ describe.skip( 'Gutenberg Editor Contact Info Block tests', () => {
 	} );
 
 	it( 'should show keyboard when selecting phone', async () => {
-		const phoneBlock = await editorPage.getBlockAtPosition( 'Phone Number', 2 );
+		const phoneBlock = await editorPage.getBlockAtPosition(
+			'Phone Number',
+			2
+		);
 		phoneBlock.click();
 
 		const keyboardShown = await driver.isKeyboardShown();
@@ -122,7 +131,10 @@ describe.skip( 'Gutenberg Editor Contact Info Block tests', () => {
 	} );
 
 	it( 'should show keyboard when selecting address', async () => {
-		const addressBlock = await editorPage.getBlockAtPosition( 'Address', 3 );
+		const addressBlock = await editorPage.getBlockAtPosition(
+			'Address',
+			3
+		);
 		addressBlock.click();
 
 		const keyboardShown = await driver.isKeyboardShown();
@@ -137,16 +149,29 @@ describe.skip( 'Gutenberg Editor Contact Info Block tests', () => {
 	} );
 
 	it( 'should have settings toggle on address block', async () => {
-		const addressBlock = await editorPage.getBlockAtPosition( 'Address', 3 );
+		const addressBlock = await editorPage.getBlockAtPosition(
+			'Address',
+			3
+		);
 		addressBlock.click();
 
-		await toggleSettings(driver, editorPage, addressBlock);
+		await toggleSettings( driver, editorPage, addressBlock );
 
-		const addressSettingsLabel = findElement(driver, editorPage, 'Address Settings', 'Label');
-		expect(addressSettingsLabel).toBeTruthy();
+		const addressSettingsLabel = findElement(
+			driver,
+			editorPage,
+			'Address Settings',
+			'Label'
+		);
+		expect( addressSettingsLabel ).toBeTruthy();
 
-		const addressSettingsLinkToggle = findElement(driver, editorPage, 'Link address to Google Maps', 'Toggle');
-		expect(addressSettingsLinkToggle).toBeTruthy();
+		const addressSettingsLinkToggle = findElement(
+			driver,
+			editorPage,
+			'Link address to Google Maps',
+			'Toggle'
+		);
+		expect( addressSettingsLinkToggle ).toBeTruthy();
 
 		await dismissActionSheet();
 		await editorPage.dismissKeyboard();
@@ -157,22 +182,57 @@ describe.skip( 'Gutenberg Editor Contact Info Block tests', () => {
 	} );
 
 	it( 'should only have phone, address, email, separator, heading and spacer options as child blocks', async () => {
-		const unavailableBlocks = [ 'Paragraph', 'More', 'Image', 'Video', 'Page Break', 'List', 'Quote', 'Media & Text', 'Preformatted', 'Gallery',
-		'Columns', 'Group', 'Shortcode', 'Buttons', 'Latest Posts', 'Verse', 'Cover', 'Social Icons', 'Pullquote' ];
-		const availableBlocks = [ 'Address', 'Email Address', 'Phone Number', 'Heading', 'Separator', 'Spacer' ];
+		const unavailableBlocks = [
+			'Paragraph',
+			'More',
+			'Image',
+			'Video',
+			'Page Break',
+			'List',
+			'Quote',
+			'Media & Text',
+			'Preformatted',
+			'Gallery',
+			'Columns',
+			'Group',
+			'Shortcode',
+			'Buttons',
+			'Latest Posts',
+			'Verse',
+			'Cover',
+			'Social Icons',
+			'Pullquote',
+		];
+		const availableBlocks = [
+			'Address',
+			'Email Address',
+			'Phone Number',
+			'Heading',
+			'Separator',
+			'Spacer',
+		];
 
-		const emailBlock = await editorPage.getBlockAtPosition( 'Email Address' );
+		const emailBlock = await editorPage.getBlockAtPosition(
+			'Email Address'
+		);
 		emailBlock.click();
 
-		const addBlockIdentifier = isAndroid() ? 'Add block, Double tap to add a block' : 'Add block';
+		const addBlockIdentifier = isAndroid()
+			? 'Add block, Double tap to add a block'
+			: 'Add block';
 		const addButton = await driver.elementByAccessibilityId(
 			addBlockIdentifier
 		);
 		await addButton.click();
 
-		for (const blockName of [ ...unavailableBlocks, ...availableBlocks ]) {
-			const block = await driver.hasElementByAccessibilityId(blockName);
-			expect(block).toBe(availableBlocks.includes(blockName));
+		// Disabling eslint for next line because there seems to be a bug where variable in for...of block isn't seen.
+		// eslint-disable-next-line no-unused-vars
+		for ( const blockName of [
+			...unavailableBlocks,
+			...availableBlocks,
+		] ) {
+			const block = await driver.hasElementByAccessibilityId( blockName );
+			expect( block ).toBe( availableBlocks.includes( blockName ) );
 		}
 
 		await dismissActionSheet();
