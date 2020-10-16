e7aa037bde5daf8968f13dcafa08349692f09632
Mario Zorz <mariozorz@gmail.com>
2020-10-16 19:30:23 -0300

    bump ci

--- src/test/index.js
+++ src/test/index.js
@@ -12,6 +12,7 @@ describe( 'Test Jetpack blocks', () => {
 			'../../jetpack/extensions/blocks/contact-info/editor.js',
 			() => jest.fn()
 		);
+		
 		jest.mock( '../../jetpack/extensions/blocks/story/editor.js', () =>
 			jest.fn()
 		);
