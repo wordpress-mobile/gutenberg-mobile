835a286851a258b48456b731d0afe683838e114d
Mario Zorz <mariozorz@gmail.com>
2020-10-30 18:51:51 -0300

    mocked useSelect, useDispatch in test

--- src/test/index.js
+++ src/test/index.js
@@ -1,5 +1,37 @@
+/**
+ * WordPress dependencies
+ */
+import { useDispatch, useSelect } from '@wordpress/data';
+// import { JetpackLogo } from '../../jetpack/extensions/shared/icons';
+// import { JetpackLogo } from '../../jetpack/extensions/shared/icons';
+
+// import { NoticeList } from '../../gutenberg/packages/components/src/notice/list';
+
+
+jest.mock( '@wordpress/data/build/components/use-select' );
+jest.mock( '@wordpress/data/build/components/use-dispatch' );
+// jest.mock('../../jetpack/extensions/shared/icons');
+
+// jest.mock('../../gutenberg/packages/components/src/notice/list');
+
 describe( 'Test Jetpack blocks', () => {
 	it( 'should setup the editor for jetpack without failing', () => {
+		useSelect.mockImplementation( () => {
+			return {
+				mediaFilesCollectionBlock: true,
+			};
+		} );
+
+		useDispatch.mockImplementation( () => {
+			return {
+				hideBlockTypes: jest.fn(),
+				showBlockTypes: jest.fn(),
+			}	
+		} );
+
+		// JetpackLogo.mockImplementation( () => { return 'JetpackLogo' }  );
+		// NoticeList.mockImplementation( () => { return 'NoticeList' }  );
+
 		const mockRegisterBlockCollection = jest.fn();
 		jest.mock( '@wordpress/blocks', () => {
 			return {
