e1e1592537690b3e0f5c5ae64717f67b5b372f85
Ceyhun Ozugur <ceyhunozugur@gmail.com>
2021-01-07 12:00:04 +0100

    Revert "Remove additional node memory when building packages"
    This reverts commit 49d0edcb4a76664a915dc3f37cdeb697c3ae0983.

--- bin/ci-checks-js.sh
+++ bin/ci-checks-js.sh
@@ -35,7 +35,7 @@ if [ "$CHECK_CORRECTNESS" = true ] ; then
   # Need to build gutenberg packages before linting so that eslint-plugin-import can resolve those.
   # See https://github.com/WordPress/gutenberg/pull/22088 for more information.
   cd gutenberg
-  npm run build:packages || pFail
+  NODE_OPTIONS=--max_old_space_size=4096 npm run build:packages || pFail
   cd ..
 
   npm run lint || pFail
