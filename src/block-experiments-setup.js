4f9339b7d7ce48d9cfaf2bda9c98846cf196b3fd
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-05-19 23:27:31 -0700

    Fix up the block setup code

--- src/block-experiments-setup.js
+++ src/block-experiments-setup.js
@@ -1,5 +1,19 @@
-// This file is to set up the jetpack/layout-grid block that currently lives in block-experiments/blocks/layout-grid
+/**
+ * WordPress dependencies
+ */
+ import { select } from '@wordpress/data';
 
-import { registerBlock } from '../block-experiments/blocks/layout-grid/src';
-
-registerBlock();
+ // This file is to set up the jetpack/layout-grid block that currently lives in block-experiments/blocks/layout-grid
+ import { registerBlock } from '../block-experiments/blocks/layout-grid/src';
+ 
+ export default function registerBlocks() {
+	 // eslint-disable-next-line @wordpress/react-no-unsafe-timeout
+	 setTimeout( () => {
+		 const capabilities = select( 'core/block-editor' ).getSettings(
+			 'capabilities'
+		 );
+		 if ( capabilities.layoutGridBlock ) {
+			 registerBlock();
+		 }
+	 } );
+ }
