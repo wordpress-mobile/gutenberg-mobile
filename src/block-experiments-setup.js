226e2c43f90e909c31a545eea7ade03c1ae4ba8e
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-06-21 14:31:58 -0700

    Fix minor prettier issue.

--- src/block-experiments-setup.js
+++ src/block-experiments-setup.js
@@ -1,8 +1,7 @@
-
 // This file is to set up the jetpack/layout-grid block that currently lives in block-experiments/blocks/layout-grid
 import { registerBlock } from '../block-experiments/blocks/layout-grid/src';
 
-export default function setupBlockExperiments( capabilities ) {	
+export default function setupBlockExperiments( capabilities ) {
 	if ( capabilities.layoutGridBlock ) {
 		registerBlock();
 	}
