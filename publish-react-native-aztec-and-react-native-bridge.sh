777002b3132f9f36cfa2208c13eb6079b8467994
Oguz Kocer <oguz.kocer@automattic.com>
2021-07-12 23:07:44 -0400

    Fix where the android bundle is copied during Buildkite publishing

--- publish-react-native-aztec-and-react-native-bridge.sh
+++ publish-react-native-aztec-and-react-native-bridge.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-VERSION=$1
+set -eo pipefail
 
-mkdir -p react-native-bridge/build/assets
-cp ../../../../bundle/android/App.js ./react-native-bridge/build/assets/index.android.bundle
+VERSION=$1
 
 pushd ./gutenberg/packages/react-native-aztec/android
 ./gradlew -PwillPublishReactNativeAztecBinary=true :prepareToPublishToS3 --tag-name="$VERSION" :publish
