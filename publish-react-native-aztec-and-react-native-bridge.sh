a47e2ead42c8be382713f436cc51218f2a6b8855
Oguz Kocer <oguz.kocer@automattic.com>
2021-07-17 04:13:26 -0400

    Adds prepare_to_publish_to_s3 params

--- publish-react-native-aztec-and-react-native-bridge.sh
+++ publish-react-native-aztec-and-react-native-bridge.sh
@@ -4,10 +4,13 @@ set -eo pipefail
 
 VERSION=$1
 
+source /scripts/prepare_to_publish_to_s3_params.sh
+PREPARE_TO_PUBLISH_TO_S3_PARAMS=$(prepare_to_publish_to_s3_params)
+
 pushd ./gutenberg/packages/react-native-aztec/android
-./gradlew -PwillPublishReactNativeAztecBinary=true :prepareToPublishToS3 --tag-name="$VERSION" :publish
+./gradlew -PwillPublishReactNativeAztecBinary=true :prepareToPublishToS3 PREPARE_TO_PUBLISH_TO_S3_PARAMS :publish
 popd
 
 pushd ./gutenberg/packages/react-native-bridge/android
-./gradlew -PwillPublishReactNativeBridgeBinary=true -PreactNativeAztecVersion="$VERSION" :react-native-bridge:prepareToPublishToS3 --tag-name="$VERSION" :react-native-bridge:publish
+./gradlew -PwillPublishReactNativeBridgeBinary=true -PreactNativeAztecVersion="$VERSION" :react-native-bridge:prepareToPublishToS3 PREPARE_TO_PUBLISH_TO_S3_PARAMS :react-native-bridge:publish
 popd
