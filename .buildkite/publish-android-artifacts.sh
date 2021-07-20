9e56603c19bf429f66b231806717f825bd90bf65
Oguz Kocer <oguz.kocer@automattic.com>
2021-07-20 12:30:31 -0400

    Revert prepare_to_publish_to_s3_params change in .buildkite/publish-android-artifacts.sh

--- .buildkite/publish-android-artifacts.sh
+++ .buildkite/publish-android-artifacts.sh
@@ -6,12 +6,11 @@ buildkite-agent artifact download bundle/android/App.js .
 mkdir -p gutenberg/packages/react-native-bridge/android/react-native-bridge/build/assets
 cp ./bundle/android/App.js ./gutenberg/packages/react-native-bridge/android/react-native-bridge/build/assets/index.android.bundle
 
-PREPARE_TO_PUBLISH_PARAMS=`prepare_to_publish_to_s3_params`
-
 pushd ./gutenberg/packages/react-native-aztec/android
-./gradlew -PwillPublishReactNativeAztecBinary=true :prepareToPublishToS3 "$PREPARE_TO_PUBLISH_PARAMS" :publish
-PUBLISHED_REACT_NATIVE_VERSION=`cat ./build/published-version.txt`
+./gradlew -PwillPublishReactNativeAztecBinary=true :prepareToPublishToS3 `prepare_to_publish_to_s3_params` :publish
+PUBLISHED_AZTEC_VERSION=`cat ./build/published-version.txt`
+echo "PUBLISHED_AZTEC_VERSION: $PUBLISHED_AZTEC_VERSION"
 popd
 
 pushd ./gutenberg/packages/react-native-bridge/android
-./gradlew -PwillPublishReactNativeBridgeBinary=true -PreactNativeAztecVersion="$PUBLISHED_REACT_NATIVE_VERSION" :react-native-bridge:prepareToPublishToS3 "$PREPARE_TO_PUBLISH_PARAMS" :react-native-bridge:publish
+./gradlew -PwillPublishReactNativeBridgeBinary=true -PreactNativeAztecVersion="$PUBLISHED_AZTEC_VERSION" :react-native-bridge:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :react-native-bridge:publish
