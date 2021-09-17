2eaed3338a05e5e717c8e8cdba33e270113835f5
Enej Bajgoric <enej.bajgoric@automattic.com>
2021-09-17 15:29:30 -0700

    Missed a spacing update

--- bundle/ios/GutenbergNativeTranslations.swift
+++ bundle/ios/GutenbergNativeTranslations.swift
@@ -22,7 +22,7 @@ private func dummy() {
     _ = NSLocalizedString("Add Block After", comment: "")
     _ = NSLocalizedString("Add Block Before", comment: "")
     _ = NSLocalizedString("ADD BLOCK HERE", comment: "")
-	_ = NSLocalizedString("Add blocks", comment: "")
+    _ = NSLocalizedString("Add blocks", comment: "")
     _ = NSLocalizedString("Add button text", comment: "")
     _ = NSLocalizedString("ADD IMAGE", comment: "")
     _ = NSLocalizedString("Add image or video", comment: "")
