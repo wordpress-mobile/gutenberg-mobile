e2129b2ea085c5f2406ee49557417f3aabf64370
jhnstn <jason@readysetandco.com>
2021-10-20 10:45:16 -0400

    Restructure to use a domain based structure

--- src/analytics/events.js
+++
@@ -1,5 +0,0 @@
-export const INSERT_BLOCK = 'insertBlock';
-
-export const REDUX_TRACKING = {
-	'core/block-editor': [ INSERT_BLOCK ],
-};
--- src/analytics/redux/index.js
+++ src/analytics/redux/index.js
@@ -2,39 +2,37 @@
  * WordPress dependencies
  */
 import { use } from '@wordpress/data';
-
 /**
  * Internal dependencies
  */
-import eventTrackers from './trackers';
-import { REDUX_TRACKING } from './events';
+import { trackedEvents } from './tracked_events';
 
 /**
  * Initialize the Redux middleware
  */
-export default function() {
+export function initialize() {
 	use( ( registry ) => ( {
 		dispatch: ( namespace ) => {
 			const namespaceName =
 				typeof namespace === 'object' ? namespace.name : namespace;
 			const actions = { ...registry.dispatch( namespaceName ) };
-			const trackedEvents = REDUX_TRACKING[ namespaceName ];
+			const trackers = trackedEvents[ namespaceName ];
 
-			if ( trackedEvents ) {
-				trackedEvents.forEach( ( actionName ) => {
+			if ( trackers ) {
+				Object.keys( trackers ).forEach( ( actionName ) => {
 					const originalAction = actions[ actionName ];
-					const tracker = eventTrackers[ actionName ];
+					const tracker = trackers[ actionName ];
 					actions[ actionName ] = ( ...args ) => {
 						try {
 							tracker( ...args );
 						} catch ( err ) {
-							// eslint-disable-next-line no-console
 							console.error( err );
 						}
 						return originalAction( ...args );
 					};
 				} );
 			}
+
 			return actions;
 		},
 	} ) );
