cfb2c246725d22c7a0c0a52b249aa4c29e05bc4a
Carlos Garcia <fluiddot@gmail.com>
2021-12-20 14:45:42 +0100

    Add note to index file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { registerGutenberg } from '@wordpress/react-native-editor';
 
 registerGutenberg( {
 	beforeInitCallback: () => {
+		// We have to lazy import the setup code to prevent executing any code located
+		// at global scope before the editor is initialized, like translations retrieval.
 		const { default: gutenbergMobileSetup } = require( './setup' );
 		gutenbergMobileSetup();
 	},
