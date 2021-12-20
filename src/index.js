fc591c96cf7156038f24440f4baa5559de68a2e7
Carlos Garcia <fluiddot@gmail.com>
2021-12-20 14:38:33 +0100

    Update index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 /**
- * Internal dependencies
+ * WordPress dependencies
  */
 import { registerGutenberg } from '@wordpress/react-native-editor';
 
-registerGutenberg( () => {
-	const { default: gutenbergMobileSetup } = require( './setup' );
-	gutenbergMobileSetup();
+registerGutenberg( {
+	beforeInitCallback: () => {
+		const { default: gutenbergMobileSetup } = require( './setup' );
+		gutenbergMobileSetup();
+	},
 } );
