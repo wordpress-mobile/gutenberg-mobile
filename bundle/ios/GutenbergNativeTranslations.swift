b015a64d501902b31a4a3dcdbc5c9160ebe65dfc
Carlos Garcia <fluiddot@gmail.com>
2021-12-30 12:27:49 +0100

    Update localization strings files

--- bundle/ios/GutenbergNativeTranslations.swift
+++ bundle/ios/GutenbergNativeTranslations.swift
@@ -156,7 +156,6 @@ private func dummy() {
     _ = NSLocalizedString("Failed to insert media.\nPlease tap for options.", comment: "")
     _ = NSLocalizedString("Failed to save files.\nPlease tap for options.", comment: "")
     _ = NSLocalizedString("Failed to upload files.\nPlease tap for options.", comment: "")
-    _ = NSLocalizedString("Featured", comment: "")
     _ = NSLocalizedString("Featured Image", comment: "")
     _ = NSLocalizedString("File block settings", comment: "")
     _ = NSLocalizedString("File name", comment: "")
