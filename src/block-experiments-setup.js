87ed8dfd704e32649c4df6bc284f447d5ecfe81c
Carlos Garcia <fluiddot@gmail.com>
2022-01-03 14:29:23 +0100

    Extract Jetpack and block experiments setup code

--- src/block-experiments-setup.js
+++ src/block-experiments-setup.js
@@ -1,11 +1,30 @@
 /**
+ * WordPress dependencies
+ */
+import { addAction } from '@wordpress/hooks';
+
+/**
  * Internal dependencies
  */
 // This file is to set up the jetpack/layout-grid block that currently lives in block-experiments/blocks/layout-grid
 import { registerBlock } from '../block-experiments/blocks/layout-grid/src';
 
-export function setupBlockExperiments( capabilities ) {
-	if ( capabilities.layoutGridBlock ) {
-		registerBlock();
-	}
-}
+const setupHooks = () => {
+	// Hook triggered after the editor is rendered
+	addAction(
+		'native.render',
+		'gutenberg-mobile-block-experiments',
+		( props ) => {
+			const capabilities = props.capabilities ?? {};
+
+			// Register Layout Grid block
+			if ( capabilities.layoutGridBlock ) {
+				registerBlock();
+			}
+		}
+	);
+};
+
+export default () => {
+	setupHooks();
+};
--- src/setup.js
+++ src/setup.js
@@ -8,31 +8,14 @@ import { initialHtmlGutenberg } from '@wordpress/react-native-editor';
  * Internal dependencies
  */
 import correctTextFontWeight from './text-font-weight-correct';
-import {
-	registerJetpackBlocks,
-	registerJetpackEmbedVariations,
-	setupJetpackEditor,
-} from './jetpack-editor-setup';
-import { setupBlockExperiments } from './block-experiments-setup';
 import initialHtml from './initial-html';
 import initAnalytics from './analytics';
 
 const setupHooks = () => {
 	// Hook triggered before the editor is rendered
-	addAction( 'native.pre-render', 'gutenberg-mobile', ( props ) => {
-		const { jetpackState } = props;
-
+	addAction( 'native.pre-render', 'gutenberg-mobile', () => {
 		require( './strings-overrides' );
 		correctTextFontWeight();
-
-		setupJetpackEditor(
-			jetpackState || { blogId: 1, isJetpackActive: true }
-		);
-
-		// Jetpack Embed variations use WP hooks that are attached to
-		// block type registration, so it’s required to add them before
-		// the core blocks are registered.
-		registerJetpackEmbedVariations( props );
 	} );
 
 	addFilter(
@@ -59,13 +42,6 @@ const setupHooks = () => {
 			return editorProps;
 		}
 	);
-
-	// Hook triggered after the editor is rendered
-	addAction( 'native.render', 'gutenberg-mobile', ( props ) => {
-		const capabilities = props.capabilities ?? {};
-		registerJetpackBlocks( props );
-		setupBlockExperiments( capabilities );
-	} );
 };
 
 export default () => {
