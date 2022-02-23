35a0a0da9b2919578d12adeaab3a9ce832171d3f
Carlos Garcia <fluiddot@gmail.com>
2022-02-23 18:39:29 +0100

    Test installing node from pipeline instead Gradle

--- .buildkite/publish-react-native-dependencies-artifacts.sh
+++ .buildkite/publish-react-native-dependencies-artifacts.sh
@@ -8,6 +8,5 @@ cd ./gutenberg/packages/react-native-bridge/android
 # ./gradlew :react-native-gesture-handler:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :react-native-gesture-handler:publish
 # ./gradlew :react-native-gesture-reanimated:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :react-native-gesture-reanimated:publish
 
-./gradlew -p android-npm npmInstall && \
 ./gradlew :react-native-gesture-handler:publishToMavenLocal :react-native-reanimated:publishToMavenLocal && \
 ls -R $HOME/.m2/repository/org/wordpress-mobile
