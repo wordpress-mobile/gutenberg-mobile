55aea73daf98a039154614e998c6ff97edb28936
Carlos Garcia <fluiddot@gmail.com>
2022-02-23 18:23:09 +0100

    Update publish dependencies artifacts script

--- .buildkite/publish-react-native-dependencies-artifacts.sh
+++ .buildkite/publish-react-native-dependencies-artifacts.sh
@@ -4,13 +4,10 @@ set -euo pipefail
 
 cd ./gutenberg/packages/react-native-bridge/android
 
-# Install npm dependencies
-./gradlew -p android-npm npmInstall
-
 # Publish Android dependencies
 # ./gradlew :react-native-gesture-handler:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :react-native-gesture-handler:publish
 # ./gradlew :react-native-gesture-reanimated:prepareToPublishToS3 `prepare_to_publish_to_s3_params` :react-native-gesture-reanimated:publish
 
-./gradlew :react-native-gesture-handler:publishToMavenLocal
-./gradlew :react-native-gesture-reanimated:publishToMavenLocal
+./gradlew -p android-npm npmInstall && \
+./gradlew :react-native-gesture-handler:publishToMavenLocal :react-native-reanimated:publishToMavenLocal && \
 ls -R $HOME/.m2/repository/org/wordpress-mobile
