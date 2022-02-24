f169ab6932f736acd356125c5bc38e8da4a56248
Carlos Garcia <fluiddot@gmail.com>
2022-02-24 12:55:50 +0100

    Fix published-version path

--- .buildkite/publish-android-dependencies-artifacts.sh
+++ .buildkite/publish-android-dependencies-artifacts.sh
@@ -4,13 +4,15 @@ set -euo pipefail
 
 cd ./gutenberg/packages/react-native-bridge/android
 
+NODE_MODULES_PATH='../../../node_modules'
+
 # Publish Android dependencies
 ./gradlew -PwillPublishAndroidDependenciesBinaries=true \
     :react-native-gesture-handler:prepareToPublishToS3 `prepare_to_publish_to_s3_params` \
     :react-native-gesture-handler:publish
-cat ./build/published-version.txt | buildkite-agent meta-data set "PUBLISHED_REACT_NATIVE_GESTURE_HANDLER_VERSION"
+cat "$NODE_MODULES_PATH/react-native-gesture-handler/android/build/published-version.txt" | buildkite-agent meta-data set "PUBLISHED_REACT_NATIVE_GESTURE_HANDLER_VERSION"
 
 ./gradlew -PwillPublishAndroidDependenciesBinaries=true \
     :react-native-reanimated:prepareToPublishToS3 `prepare_to_publish_to_s3_params` \
     :react-native-reanimated:publish
-cat ./build/published-version.txt | buildkite-agent meta-data set "PUBLISHED_REACT_NATIVE_REANIMATED_VERSION"
+cat "$NODE_MODULES_PATH/react-native-reanimated/android/build/published-version.txt" | buildkite-agent meta-data set "PUBLISHED_REACT_NATIVE_REANIMATED_VERSION"
