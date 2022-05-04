3bfc9e672f37d2145df3db983e3cf7b05477d322
jhnstn <jason@readysetandco.com>
2022-05-04 17:13:00 -0400

    fix unbound CIRCLE_JOB when running install locally

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -6,7 +6,7 @@ pushd jetpack >/dev/null
 pnpm_version=$(npx -c 'echo "$npm_package_engines_pnpm"')
 
 # Restore the public hoisting if running on circle CI
-if [[ -n "$CIRCLE_JOB" ]]; then
+if [[ -n "${CIRCLE_JOB:-}" ]]; then
   sed -i.bak '/hoist/d' .npmrc
   echo "public-hoist-pattern=['*types*', '@prettier/plugin-*', '*prettier-plugin-*']" >> .npmrc
 fi
