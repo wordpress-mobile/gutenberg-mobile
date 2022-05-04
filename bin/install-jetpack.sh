ee139ed7cb70b748b87c62e10a23b496c3435e7a
jhnstn <jason@readysetandco.com>
2022-05-04 16:39:17 -0400

    Try overriding hoisting on ci only

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -5,6 +5,14 @@ pushd jetpack/projects/plugins/jetpack >/dev/null
 
 pnpm_version=$(npx -c 'echo "$npm_package_engines_pnpm"')
 
-npx pnpm@$pnpm_version install
+
+# Restore the public hoisting if running on circle CI
+if [[ -n "$CIRCLE_JOB" ]]; then
+  # Restore pnpm hoisting
+  gwak -i inplace '!/hoist/' .npmrc
+  echo "public-hoist-pattern=['*types*', '@prettier/plugin-*', '*prettier-plugin-*']" >> .npmrc
+fi
+
+npx pnpm@"$pnpm_version" install
 
 popd >/dev/null
