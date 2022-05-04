83cbafdf6ae0f844c233e4574152747a9fe5db31
jhnstn <jason@readysetandco.com>
2022-05-04 17:08:59 -0400

    Update .npmrc while in the project root

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-pushd jetpack/projects/plugins/jetpack >/dev/null
+pushd jetpack />/dev/null
 
 pnpm_version=$(npx -c 'echo "$npm_package_engines_pnpm"')
 
-ls -l
 # Restore the public hoisting if running on circle CI
 if [[ -n "$CIRCLE_JOB" ]]; then
   sed -i.bak '/hoist/d' .npmrc
   echo "public-hoist-pattern=['*types*', '@prettier/plugin-*', '*prettier-plugin-*']" >> .npmrc
 fi
-
+cd projects/plugins/jetpack
 npx pnpm@"$pnpm_version" install
 
 popd >/dev/null
