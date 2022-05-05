2f27ec212561d486e67f1184181cb534d4614c6d
jhnstn <jason@readysetandco.com>
2022-05-04 23:05:51 -0400

    Add Jetpack install script

---
+++ bin/install-jetpack.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+pushd jetpack >/dev/null
+
+pnpm_version=$(npx -c 'echo "$npm_package_engines_pnpm"')
+
+# Restore the public hoisting if running on CI
+if [[ -n "${CI:-}" ]]; then
+  sed -i.bak '/hoist/d' .npmrc
+  #echo "public-hoist-pattern=['*types*', '@prettier/plugin-*', '*prettier-plugin-*']" >> .npmrc
+fi
+cd projects/plugins/jetpack
+npx pnpm@"$pnpm_version" install
+
+popd >/dev/null
