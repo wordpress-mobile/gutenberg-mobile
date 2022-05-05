6c2c3d8368e22dd962e5aec59c4890ed918234fc
jhnstn <jason@readysetandco.com>
2022-05-04 23:36:11 -0400

    update how .npmrc is cleaned up

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -5,18 +5,18 @@ pushd jetpack
 
 pnpm_version=$(npx -c 'echo "$npm_package_engines_pnpm"')
 
-
-# Restore the public hoisting if running on CI
+# Remove pnpm hoisting config if on CI
 if [[ -n "${CI:-}" ]]; then
-  sed -i '/hoist/d' .npmrc
+  sed -i.bk '/hoist/d' .npmrc
 fi
-pushd projects/plugins/jetpack
-npx pnpm@"$pnpm_version" install
 
-# Clean up the .npmrc if on CI
+cd projects/plugins/jetpack
+npx pnpm@"$pnpm_version" install
+cd -
+
+# Retore .npmrc if on CI
 if [[ -n "${CI:-}" ]]; then
-  git checkout .npmrc
-  popd
+  mv -f .npmrc.bk .npmrc
 fi
 
 popd
