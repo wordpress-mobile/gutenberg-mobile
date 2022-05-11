52afdaf3066789827b2309853f8a7e1f40284bcd
jhnstn <jason@readysetandco.com>
2022-05-10 23:50:09 -0400

    Look for pnpm packages

--- metro.config.js
+++ metro.config.js
@@ -15,27 +15,47 @@ const gutenbergMetroConfigCopy = {
 
 const nodeModulePaths = [
 	'gutenberg/node_modules',
-	'./jetpack/node_modules/.pnpm/node_modules',
-].map( ( dir ) => path.join( process.cwd(), dir ) );
+	'jetpack/projects/plugins/jetpack/node_modules',
+];
 
 const possibleModulePath = ( name ) =>
-	nodeModulePaths.map( ( dir ) => path.join( dir, name ) );
+	nodeModulePaths.map( ( dir ) => path.join( process.cwd(), dir, name ) );
+
+const packagedByPnpm = ( dir ) => dir && dir.includes( '.pnpm' );
 
 gutenbergMetroConfigCopy.resolver.resolveRequest = (
 	context,
 	moduleName,
 	platform
 ) => {
-	if ( moduleName[ 0 ] !== '.' ) {
+	if ( ! ( moduleName.startsWith( '.' ) || moduleName.startsWith( '/' ) ) ) {
 		const [ namespace, module = '' ] = moduleName.split( '/' );
 		const name = path.join( namespace, module );
 
 		if ( ! extraNodeModules[ name ] ) {
-			const modulePath = possibleModulePath( name ).find( fs.existsSync );
+			let extraNodeModulePath;
 
+			const modulePath = possibleModulePath( name ).find( fs.existsSync );
 			if ( modulePath ) {
-				extraNodeModules[ name ] = fs.realpathSync( modulePath );
+				extraNodeModulePath = fs.realpathSync( modulePath );
 			}
+
+			if ( ! extraNodeModulePath ) {
+				// Check if package is managed by pnpm
+				const filePath = require.resolve( name, {
+					paths: [ path.dirname( context.originModulePath ) ],
+				} );
+
+				const innerNodeModules = filePath.match(
+					/.*node_modules/
+				)?.[ 0 ];
+
+				if ( packagedByPnpm( innerNodeModules ) ) {
+					extraNodeModulePath = path.join( innerNodeModules, name );
+				}
+			}
+
+			extraNodeModules[ name ] = extraNodeModulePath;
 		}
 	}
 
