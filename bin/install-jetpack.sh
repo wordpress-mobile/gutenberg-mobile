e4c0df871bfda34d106f960b5dfdb5c05b71b31c
Jason Johnston <jhnstn@users.noreply.github.com>
2022-05-24 14:48:28 -0400

    More clean up

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -2,26 +2,25 @@
 set -Eeuo pipefail
 
 # Check if nvm is installed
-if ! command -v nvm ; then
-  source ~/.nvm/nvm.sh &>/dev/null
-  if [ $? -ne 0 ]; then
-    echo "nvm is not installed or cannot be sourced from ~/.nvm/nvm.sh. Please install nvm and run this script again."
-    exit 1
-  fi
-fi
+[[ -f ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
 
-# Set up node requirement for Jetpack
+command -v nvm >/dev/null 2>&1 || {
+  echo "nvm is not installed or cannot be sourced from ~/.nvm/nvm.sh. Please install nvm and run this script again."
+  exit 1
+}
+
 pushd jetpack
 
-node_version=$(cat .nvmrc)
-nvm install "$node_version"
-nvm use
+# Set up node requirement for Jetpack
+nvm install
 
 # Set up required pnpm version
 listed_pnpm_version=$(npx -c 'echo $npm_package_engines_pnpm')
 pnpm_version=$(npx semver -c "$listed_pnpm_version")
 
 cd projects/plugins/jetpack
+
+# npx might prompt to install pnpm at the requested version. Let's just agree and carry on.
 ( yes || true ) | npx pnpm@"$pnpm_version" install
 
 popd
