2a5d24138bf38eb17a06cf4d0e524508da7823b8
Jason Johnston <jhnstn@users.noreply.github.com>
2022-06-10 19:55:13 -0400

    might need to accept installing pnpm

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -16,17 +16,17 @@ pushd jetpack
 nvm install
 
 npm view pnpm versions --json
-command -v pnpm >/dev/null 2>&1 || echo "pnpm is not available, not surprised"
-npx pnpm@7 -v || echo "hmm it looks like 7.1.1 is not available ? "
-npm install pnpm@7.1.1
+yes | npx pnpm@7 -v || echo "7 is not available"
+yes | npx pnpm@6 -v || echo "6 is not available"
 
 # Set up required pnpm version
 listed_pnpm_version=$(npx -c 'echo $npm_package_engines_pnpm')
 pnpm_version=$(npx semver -c "$listed_pnpm_version")
+major=$(cut -d '.' -f 1 <<< "$pnpm_version")
 
 cd projects/plugins/jetpack
 
 # npx might prompt to install pnpm at the requested version. Let's just agree and carry on.
-npx pnpm@7 install
+yes | npx pnpm@"$major" install
 
 popd
