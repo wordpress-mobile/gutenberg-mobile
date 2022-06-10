24fba2b85486da366371305b64f408db136b74c5
Jason Johnston <jhnstn@users.noreply.github.com>
2022-06-10 17:56:50 -0400

    Try debugging npm + pnpm

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -15,6 +15,10 @@ pushd jetpack
 # Set up node requirement for Jetpack
 nvm install
 
+npm view pnpm versions --json
+
+npmx pnpm@7.1.1 -v
+
 # Set up required pnpm version
 listed_pnpm_version=$(npx -c 'echo $npm_package_engines_pnpm')
 pnpm_version=$(npx semver -c "$listed_pnpm_version")
@@ -22,6 +26,6 @@ pnpm_version=$(npx semver -c "$listed_pnpm_version")
 cd projects/plugins/jetpack
 
 # npx might prompt to install pnpm at the requested version. Let's just agree and carry on.
-( yes || true ) | npx pnpm@"$pnpm_version" install
+npx pnpm@"$pnpm_version" install
 
 popd
