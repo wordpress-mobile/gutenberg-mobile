567bec95da52012de3b9433a1b6fa84ae3e557a4
Jason Johnston <jhnstn@users.noreply.github.com>
2022-06-10 20:18:55 -0400

    Try the experimental corepack

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -15,19 +15,16 @@ pushd jetpack
 # Set up node requirement for Jetpack
 nvm install
 
-npm view pnpm versions --json
-yes | npx pnpm@7 -v || echo "7 is not available"
-yes | npx pnpm@6 -v || echo "6 is not available"
-npx pnpm@6.32.3 -v || echo "6.32.3 is not available???"
-
 # Set up required pnpm version
 listed_pnpm_version=$(npx -c 'echo $npm_package_engines_pnpm')
 pnpm_version=$(npx semver -c "$listed_pnpm_version")
-major=$(cut -d '.' -f 1 <<< "$pnpm_version")
+
+corepack enable
+corepack prepare pnpm@<version> --activate
 
 cd projects/plugins/jetpack
 
 # npx might prompt to install pnpm at the requested version. Let's just agree and carry on.
-yes | npx pnpm@"$major" install
+pnpm install
 
 popd
