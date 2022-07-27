0b1c5094ce5005d15ddd4e3f1046e3a9ec475093
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-07-27 13:09:14 -0600

    Add actual work

--- .buildkite/commands/build-mac-bundle.sh
+++ .buildkite/commands/build-mac-bundle.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -eu
 
-# Install NVM
+# Install NVM (and the default node)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.nvm/nvm.sh --install
 
-nvm install
+echo -e "--- :npm: Installing Dependencies"
+npm install
 
-# make build-ios-for-testing
-# make e2e-test-ios
+echo -e "--- :xcode: Building"
+npm run core test:e2e:build-app:ios
