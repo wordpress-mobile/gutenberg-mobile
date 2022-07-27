3631943831827334117d26649e6f3122163361c4
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2022-07-27 14:05:02 -0600

    Try NVM stuff

--- .buildkite/commands/build-mac-bundle.sh
+++ .buildkite/commands/build-mac-bundle.sh
@@ -4,9 +4,13 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.nvm/nvm.sh --install
 
+nvm install
+nvm alias default $(nvm current)
+
 echo -e "--- :npm: Installing Dependencies"
 npm install
 
 echo -e "--- :xcode: Building"
 gem install bundler:2.1.4
 npm run core test:e2e:build-app:ios
+npm run test:e2e:bundle:ios
