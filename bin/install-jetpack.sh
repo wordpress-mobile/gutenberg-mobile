edd121d8255bd6c8202c775986c1fed9ea00cc38
Gerardo <gerardo.pacheco@automattic.com>
2023-01-02 17:55:16 +0100

    Update script

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -20,8 +20,9 @@ pushd jetpack
 
 # Set up node requirement for Jetpack
 nvm install
-# Enforce to use the default version in the rest of workflow
-echo "$BASH_ENV"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Set up required pnpm version
 listed_pnpm_version=$(npx -c 'echo $npm_package_engines_pnpm')
