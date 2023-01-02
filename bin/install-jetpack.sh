c9cfed28535be1ddf20e865c1524790d10625eed
Gerardo <gerardo.pacheco@automattic.com>
2023-01-02 15:04:10 +0100

    Update script

--- bin/install-jetpack.sh
+++ bin/install-jetpack.sh
@@ -20,27 +20,20 @@ pushd jetpack
 
 # Set up node requirement for Jetpack
 nvm install
+nvm install-latest-npm
 
 # Set up required pnpm version
 listed_pnpm_version=$(npx -c 'echo $npm_package_engines_pnpm')
 pnpm_version=$(npx semver -c "$listed_pnpm_version")
 
-NPM_PREFIX=$(which npm | sed 's/\/bin\/npm//g')
-
-# Set npm correct prefix
-# npm config set prefix $NPM_PREFIX
-npm config set prefix /opt/circleci/.nvm/versions/node/v16.17.0
-
 # Install pnpm 
 echo "Installing pnpm $pnpm_version globally"
 npm install -g pnpm@"$pnpm_version"
 
-echo "get current prefix: $(npm config get prefix)"
+echo "Get current prefix: $(npm config get prefix)"
 
-echo "Installed Jetpack"
+echo "Install Jetpack"
 echo "pnpm path: $(which pnpm)"
-echo "Circle CI string: $NPM_PREFIX"
 pnpm install
-
 
 popd
