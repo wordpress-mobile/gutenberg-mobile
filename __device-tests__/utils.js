5e6fb001c8b75829940c494400ccccc2a3c3f237
Gerardo <gerardo.pacheco@automattic.com>
2023-02-22 09:01:43 +0100

    E2E Utils - Update takeScreenshot to take into account the device orientation

--- __device-tests__/utils.js
+++ __device-tests__/utils.js
@@ -6,8 +6,10 @@ import jimp from 'jimp';
 const { isAndroid } = e2eUtils;
 
 export async function takeScreenshot() {
+	const orientation = await editorPage.driver.getOrientation();
 	const statusBarHeight = isAndroid() ? 100 : 94;
 	const screenshot = await editorPage.driver.takeScreenshot();
+	const widthSize = orientation === 'PORTRAIT' ? 320 : 640;
 
 	const base64Image = Buffer.from( screenshot, 'base64' );
 	const image = await jimp.read( base64Image );
@@ -17,6 +19,6 @@ export async function takeScreenshot() {
 		image.getWidth(),
 		image.getHeight() - statusBarHeight
 	);
-	const resizedImage = await image.resize( 320, jimp.AUTO );
+	const resizedImage = await image.resize( widthSize, jimp.AUTO );
 	return resizedImage.getBufferAsync( jimp.MIME_PNG );
 }
