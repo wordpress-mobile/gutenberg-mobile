507f9a3764e55d6bf42dbbfab4b246cdce21084d
Gerardo <gerardo.pacheco@automattic.com>
2023-03-09 11:55:25 +0100

    Add Jetpack mock

--- jest_setup.js
+++ jest_setup.js
@@ -4,3 +4,14 @@ global.window.JP_CONNECTION_INITIAL_STATE = {};
 jest.mock( './jetpack/projects/js-packages/config/src', () => ( {
 	__esModule: true,
 } ) );
+
+jest.mock(
+	'./jetpack/projects/js-packages/shared-extension-utils/index.js',
+	() => ( {
+		__esModule: true,
+		...jest.requireActual(
+			'./jetpack/projects/js-packages/shared-extension-utils/index.js'
+		),
+		getHostAppNamespace: jest.fn(),
+	} )
+);
