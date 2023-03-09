29521810d3368f3f0a6b45f07a66cf8d42d78412
Gerardo <gerardo.pacheco@automattic.com>
2023-03-09 14:30:16 +0100

    Update Jetpack Initial state mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,17 @@
 // Jetpack mocks
-global.window.JP_CONNECTION_INITIAL_STATE = {};
+
+// Copied from https://github.com/Automattic/jetpack/blob/trunk/projects/js-packages/connection/jest.setup.js
+global.window.JP_CONNECTION_INITIAL_STATE = {
+	userConnectionData: {
+		currentUser: {
+			wpcomUser: {
+				Id: 99999,
+				login: 'bobsacramento',
+				display_name: 'Bob Sacrmaneto',
+			},
+		},
+	},
+};
 
 jest.mock(
 	'./jetpack/projects/js-packages/shared-extension-utils/index.js',
