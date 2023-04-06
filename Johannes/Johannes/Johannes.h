484bc79addfc619c28a9bb3cf36422275a2372cb
Gio Lodi <gio@mokacoding.com>
2023-04-06 20:51:03 +1000

    WIP – Project to try and create framework for Gutenberg

---
+++ Johannes/Johannes/Johannes.h
@@ -0,0 +1,18 @@
+//
+//  Johannes.h
+//  Johannes
+//
+//  Created by Gio Lodi on 6/4/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Johannes.
+FOUNDATION_EXPORT double JohannesVersionNumber;
+
+//! Project version string for Johannes.
+FOUNDATION_EXPORT const unsigned char JohannesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Johannes/PublicHeader.h>
+
+
