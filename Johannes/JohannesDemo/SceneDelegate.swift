1054ac2caad5843baf912ec69d766c25bc99ebbb
Gio Lodi <gio@mokacoding.com>
2023-04-11 20:36:32 +1000

    Ditch the Storyboard in Johannes demo app

--- Johannes/JohannesDemo/SceneDelegate.swift
+++ Johannes/JohannesDemo/SceneDelegate.swift
@@ -5,6 +5,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+
+        let rootViewController = ViewController()
+
+        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+        window?.makeKeyAndVisible()
     }
 }
--- Johannes/JohannesDemo/ViewController.swift
+++ Johannes/JohannesDemo/ViewController.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-class ViewController: UIViewController {}
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
