b9341d701fe8e70ecd09e7e7337fa80ca29da732
Gio Lodi <gio@mokacoding.com>
2023-05-08 23:14:23 +1000

    `cd` back to project root after XCFramework build finishes

--- .buildkite/publish-react-native-ios-artifacts.sh
+++ .buildkite/publish-react-native-ios-artifacts.sh
@@ -14,3 +14,6 @@ HOMEBREW_NO_AUTO_UPDATE=1 HOMEBREW_NO_INSTALL_CLEANUP=1 brew install xcbeautify
 
 echo "--- :xcode: Build XCFramework"
 ./build.sh
+
+# Restore initial the location in the filesystem location
+cd ..
