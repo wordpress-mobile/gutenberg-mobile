974806646823a254110085cc3c5e4f77392e3a16
Siobhan <siobhan@automattic.com>
2023-05-10 11:04:40 +0100

    refactor: Align JSDocs comment

--- src/test/videopress/local-helpers/utils.js
+++ src/test/videopress/local-helpers/utils.js
@@ -53,11 +53,11 @@ export const pressSettingInPanel = async ( screen, panel, setting ) => {
 /**
  * Presses a setting in a specified panel, triggers a picker, and selects an option from the sheet.
  *
- * @param {Object}   screen   - The editor's screen.
- * @param {string}   panel    - The panel containing the setting to be pressed.
- * @param {string}   setting  - The setting to be pressed, which triggers the picker.
- * @param {string[]} options  - An array of all the options available in the picker.
- * @param {string}   option   - The option to be selected from the picker.
+ * @param {Object}   screen  - The editor's screen.
+ * @param {string}   panel   - The panel containing the setting to be pressed.
+ * @param {string}   setting - The setting to be pressed, which triggers the picker.
+ * @param {string[]} options - An array of all the options available in the picker.
+ * @param {string}   option  - The option to be selected from the picker.
  */
 export const pressSettingInPicker = async (
 	screen,
