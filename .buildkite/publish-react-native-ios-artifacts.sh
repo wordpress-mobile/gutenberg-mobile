163d7ca7e7d9bd78220f10651ebc0939b0797eba
Gio Lodi <gio@mokacoding.com>
2023-06-02 17:28:29 +1000

    HACK – Hack automation to test commit publishing fast

--- .buildkite/publish-react-native-ios-artifacts.sh
+++ .buildkite/publish-react-native-ios-artifacts.sh
@@ -1,24 +1,6 @@
 #!/bin/bash -eu
 
-echo "--- :arrow_down: Download iOS JS bundle"
-buildkite-agent artifact download bundle/ios/App.js .
-
-echo "--- :rubygems: Setting up Gems"
 cd ./ios-xcframework
-
 install_gems
-
-echo "--- :cocoapods: Setting up Pods"
-install_cocoapods
-
-echo "--- 🚧 Install xcbeautify formatter while not on the VM image"
-brew install xcbeautify
-
-echo "--- :xcode: Build XCFramework"
-./build.sh
-
 echo "--- :s3: Uploading XCFramework to S3"
-bundle exec fastlane upload_xcframework_to_s3
-
-# Restore initial the location in the filesystem location
-cd ..
+bundle exec fastlane t
