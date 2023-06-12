57f293ffbd04ee015c047b0cf0998a3d15e498d5
Gio Lodi <gio@mokacoding.com>
2023-06-12 20:43:56 +1000

    Use A8c CI toolkit utils for cache

--- .buildkite/download-caches.js
+++ .buildkite/download-caches.js
@@ -6,8 +6,7 @@ const cachesToUpload = JSON.parse(jsonContent);
 
 cachesToUpload.forEach(item => {
   console.log(execSync(`echo "--- :arrow_down: Download ${item.display_name}"`, { encoding: 'utf8' }));
-  console.log(execSync(`buildkite-agent artifact download ${item.archive_name} .`, { encoding: 'utf8' }));
-  console.log(execSync(`tar --extract --gzip --file=${item.archive_name} --directory=${item.folder_to_archive_basedir} ${item.folder_to_archive}`, { encoding: 'utf8' }));
+  console.log(execSync(`restore_cache $(hash_directory ${item.folder_to_archive_basedir}/${folder_to_archive}`, { encoding: 'utf8' }));
 });
 
 // Some of the cache data needs to be connected to the submodule projects.
--- .buildkite/upload-caches.js
+++ .buildkite/upload-caches.js
@@ -6,6 +6,6 @@ const cachesToUpload = JSON.parse(jsonContent);
 
 cachesToUpload.forEach(item => {
   console.log(execSync(`echo "--- :arrow_up: Upload ${item.display_name} cache"`, { encoding: 'utf8' }));
-  console.log(execSync(`tar --create --gzip --file=${item.archive_name} --directory=${item.folder_to_archive_basedir} ${item.folder_to_archive}`, { encoding: 'utf8' }));
-  console.log(execSync(`buildkite-agent artifact upload ${item.archive_name}`, { encoding: 'utf8' }));
+  let archive_path = `${item.folder_to_archive_basedir}/${item.folder_to_archive}`
+  console.log(execSync(`save_cache ${archive_path} $(hash_directory ${archive_path})`, { encoding: 'utf8' }));
 });
