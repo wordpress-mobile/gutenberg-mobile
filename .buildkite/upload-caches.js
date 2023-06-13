eac9c9c0b6b8aead69fb492c0ece8ea8543d294b
Gio Lodi <gio@mokacoding.com>
2023-06-13 19:33:33 +1000

    Add some `echo` to debug setup

--- .buildkite/upload-caches.js
+++ .buildkite/upload-caches.js
@@ -8,5 +8,7 @@ cachesToUpload.forEach(item => {
   console.log(execSync(`echo "--- :arrow_up: Upload ${item.display_name} cache"`, { encoding: 'utf8' }));
   // cd into the basedir for the folder to archive because of the folder structure the cache archive use and the fact that not all folders are in the project tree
   console.log(execSync(`cd ${item.folder_to_archive_basedir}`, { encoding: 'utf8' }));
+  console.log(execSync(`echo $(pwd)`, { encoding: 'utf8' }));
+  console.log(execSync(`ls -al`, { encoding: 'utf8' }));
   console.log(execSync(`save_cache ${item.folder_to_archive} $(hash_directory ${item.folder_to_archive})`, { encoding: 'utf8' }));
 });
