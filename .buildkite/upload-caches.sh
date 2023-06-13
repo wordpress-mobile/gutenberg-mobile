caf29a7cd3e73f471ecd0710b550c97ba53828e3
Gio Lodi <gio@mokacoding.com>
2023-06-13 22:13:22 +1000

    Add checks in upload caches script
    See failure at
    https://buildkite.com/automattic/gutenberg-mobile/builds/6139#0188b49f-5108-4186-aada-0fff4ce4a099

--- .buildkite/upload-caches.sh
+++ .buildkite/upload-caches.sh
@@ -6,11 +6,19 @@ set -o pipefail
 source .buildkite/compute-cache-key.sh
 
 jq -c '.[]' .buildkite/caches.json | while read -r item; do
-  display_name=$(echo "$item" | jq -r '.display_name')
-  folder_to_archive=$(echo "$item" | jq -r '.folder_to_archive')
   folder_to_archive_basedir=$(echo "$item" | jq -r '.folder_to_archive_basedir')
+  if [[ ! -d $folder_to_archive_basedir ]]; then
+    echo "Could not find folder $folder_to_archive_basedir"
+  fi
 
+  folder_to_archive=$(echo "$item" | jq -r '.folder_to_archive')
+  if [[ ! -d $folder_to_archive ]]; then
+    echo "Could not find folder $folder_to_archive"
+  fi
+
+  display_name=$(echo "$item" | jq -r '.display_name')
   echo "--- :arrow_up: Upload $display_name cache"
+
   pushd "$folder_to_archive_basedir"
   key=$(compute_cache_key "$folder_to_archive_basedir" "$folder_to_archive")
   # Force only for one run
