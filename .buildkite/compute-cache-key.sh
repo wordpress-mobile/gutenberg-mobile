66d5886cb34435e123467646519567902b281176
Gio Lodi <gio@mokacoding.com>
2023-06-14 10:30:04 +1000

    Add Gutenberg and Jetpack `package-lock.json` to cache key

--- .buildkite/compute-cache-key.sh
+++ .buildkite/compute-cache-key.sh
@@ -3,8 +3,11 @@
 ARCHITECTURE=$(uname -m)
 NODE_VERSION=$(node --version)
 PACKAGE_HASH=$(hash_file package-lock.json)
-# TODO: add gutenberg and jetpack package hashes
-CACHE_KEY_ROOT="$BUILDKITE_PIPELINE_SLUG-$ARCHITECTURE-node$NODE_VERSION-$PACKAGE_HASH"
+# This repo depends on the packages in its gutenberg/ and jetpack/ submodules, too
+GUTENBERG_PACKAGE_HASH=$(hash_file gutenberg/package-lock.json)
+JETPACK_PACKAGE_HASH=$(hash_file jetpack/pnpm-lock.yaml)
+
+CACHE_KEY_ROOT="$BUILDKITE_PIPELINE_SLUG-$ARCHITECTURE-node$NODE_VERSION-$PACKAGE_HASH-$GUTENBERG_PACKAGE_HASH-$JETPACK_PACKAGE_HASH"
 
 compute_cache_key() {
   local folder_to_archive_basedir="$1"
