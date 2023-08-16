4c4806c5f73144b4d63b453fb24182dfaa71b1e6
Siobhan <siobhan@automattic.com>
2023-08-16 16:02:12 +0100

    fix: Update misspelled file name

--- .buildkite/publish-react-native-ios-artifacts.sh
+++ .buildkite/publish-react-native-ios-artifacts.sh
@@ -3,7 +3,7 @@
 echo "--- :arrow_down: Download iOS JS bundle"
 buildkite-agent artifact download bundle/ios/App.js .
 buildkite-agent artifact download ios-assets.tar.gz .
-tar -xzvf assets.tar.gz -C ios-xcframework/Gutenberg/Resources/
+tar -xzvf ios-assets.tar.gz -C ios-xcframework/Gutenberg/Resources/
 
 echo '--- :node: Setup node_modules for RNReanimated'
 echo '--- :node: 1. Install nvm'
