16ef7166a16f1639b94745523dd656b491094860
Gio Lodi <gio@mokacoding.com>
2023-08-30 17:00:40 +1000

    Debug – Remove `IS_VM_HOST` `echo`
    It's unbound in the context of the `command` script. Besides, I already
    learned what I needed from it.

--- .buildkite/build-ios.sh
+++ .buildkite/build-ios.sh
@@ -1,15 +1,5 @@
 #!/bin/bash -eu
 
-# This build failed because the git-s3-cache plugin didn't checkout the repo due to finding the env var set
-# https://buildkite.com/automattic/gutenberg-mobile/builds/7033#018a447e-aca5-4b07-ba2d-3ea961873d8d
-#
-# Update: The build where this echo was added failed in the same way.
-# https://buildkite.com/automattic/gutenberg-mobile/builds/7034#018a4487-399e-41ed-a6f5-57630ed99763
-#
-# My next guess is that the problem is with the nvm plugin running before we have a chance to pass the job to the macOS VM
-echo '+++ :bug: Check IS_VM_HOST value'
-echo "$IS_VM_HOST"
-
 # This is what I would have liked to do thanks to the nvm plugin
 #
 # echo '--- :npm: Setup Node dependencies'
