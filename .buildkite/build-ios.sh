97b75de5a506d0112899ea183815b86bfc15dca3
Gio Lodi <gio@mokacoding.com>
2023-08-31 14:51:04 +1000

    Add step to upload app to SauceLabs 🤞

--- .buildkite/build-ios.sh
+++ .buildkite/build-ios.sh
@@ -40,8 +40,17 @@ npm run core test:e2e:build-app:ios
 echo '--- :react: Build iOS bundle for E2E testing'
 npm run test:e2e:bundle:ios
 
-echo '--- :compression: Prepare artifact for SauceLab upload'
+echo '--- :compression: Prepare artifact for SauceLabs upload'
 WORK_DIR=$(pwd) \
   && pushd ./gutenberg/packages/react-native-editor/ios/build/GutenbergDemo/Build/Products/Release-iphonesimulator \
   && zip -r "$WORK_DIR/gutenberg/packages/react-native-editor/ios/GutenbergDemo.app.zip" GutenbergDemo.app \
   && popd
+
+echo '--- :saucelabs: Upload app artifact to SauceLabs'
+SAUCE_FILENAME=${BUILDKITE_BRANCH//[\/]/-}
+curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" \
+  --location \
+  --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
+  --form 'payload=@"./gutenberg/packages/react-native-editor/ios/GutenbergDemo.app.zip"' \
+  --form "name=Gutenberg-$SAUCE_FILENAME.app.zip" \
+  --form 'description="Gutenberg"'
