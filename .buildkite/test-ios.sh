e253377515cb4f2300e27a981535f7004b08545c
Gio Lodi <gio@mokacoding.com>
2023-09-11 12:02:12 +1000

    Add Buildkite pipeline with iOS Sauce Labs tests

---
+++ .buildkite/test-ios.sh
@@ -0,0 +1,79 @@
+#!/bin/bash -eu
+
+MODE="iphone"
+INPUT="${1-}"
+while [ "$INPUT" != "" ]; do
+    case $INPUT in
+        --canary )
+            MODE="canary"
+            ;;
+        --ipad )
+            MODE="ipad"
+            ;;
+        * )
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+    INPUT="${1-}"
+done
+
+echo '--- :node: Setup Node depenendencies'
+echo '--- :node: 1. Install nvm'
+brew install nvm
+
+echo '--- :node: 2. Load nvm in the current shell'
+export NVM_DIR="$HOME/.nvm"
+mkdir -p "$NVM_DIR"
+[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" --install
+
+echo '--- :node: 3. Install node version from .nvmrc'
+nvm install "$(cat .nvmrc)" && nvm use
+
+echo '--- :node: 4. nmp ci (for E2E testing)'
+npm ci --prefer-offline --no-audit --ignore-scripts
+npm ci --prefix gutenberg --prefer-offline --no-audit
+
+echo '--- :ios: Set env var for iOS E2E testing'
+set -x
+export TEST_RN_PLATFORM=ios
+export TEST_ENV=sauce
+export JEST_JUNIT_OUTPUT_FILE="reports/test-results/ios-test-results.xml"
+# Set a destination different from the hardcoded one which only works in the
+# older Xcode-setup used by CircleCI
+export RN_EDITOR_E2E_IOS_DESTINATION='platform=iOS Simulator,name=iPhone 13,OS=16.4'
+# This is a relic of the CircleCI setup.
+# It should be removed once the migration to Buildkite is completed.
+export CIRCLE_BRANCH=${BUILDKITE_BRANCH}
+set +x
+
+if [ "$MODE" == 'canary' ]; then
+    SECTION='--- :saucelabs: Test iOS Canary Pages'
+    TESTS_CMD='device-tests-canary'
+elif [ "$MODE" == "ipad" ]; then
+    SECTION='--- :saucelabs: Test iOS iPad'
+    TESTS_CMD='device-tests-ipad'
+else
+    SECTION='--- :saucelabs: Test iOS iPhone'
+    TESTS_CMD='device-tests'
+fi
+
+set +e
+echo "$SECTION"
+npm run "$TESTS_CMD"
+TESTS_EXIT_CODE=$?
+set -e
+
+REPORT_SECTION_NAME='🚦 Report Tests Status'
+if [[ $TESTS_EXIT_CODE -eq 0 ]]; then
+    echo "--- $REPORT_SECTION_NAME"
+    echo "npm run $TESTS_CMD passed. 🎉"
+else
+    echo "+++ $REPORT_SECTION_NAME"
+    echo "npm run $TESTS_CMD failed."
+    echo "For more details about the failed tests, check the Buildkite annotation, the logs under the '$SECTION' section and the tests results in the artifacts tab."
+
+    annotate_test_failures "$JEST_JUNIT_OUTPUT_FILE" --slack "build-and-ship"
+    exit $TESTS_EXIT_CODE
+fi
