45726f63fb204ccccd78f2ebd01e84342315b76b
Gio Lodi <gio@mokacoding.com>
2023-09-12 16:30:47 +1000

    Add step to build Android app for Sauce Labs

---
+++ .buildkite/commands/build-android.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+
+source /root/.bashrc
+
+echo '--- :node: Setup Node environment'
+nvm install && nvm use
+
+echo '--- :npm: Install Node dependencies'
+npm ci --prefer-offline --no-audit
+
+echo '--- :android: Set env var for Android E2E testing'
+set -x
+export TEST_RN_PLATFORM=android
+export TEST_ENV=sauce
+set +x
+
+echo '--- :react: Bundle Android app for E2E testing'
+npm run test:e2e:bundle:android
+
+echo '--- :react: Build APK for Sauce Labs'
+npm run core test:e2e:build-app:android
+
+echo '--- :saucelabs: Upload APK to Suace Labs'
+SAUCE_FILENAME=${BUILDKITE_BRANCH//[\/]/-}
+curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" \
+  --location \
+  --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
+  --form 'payload=@"./gutenberg/packages/react-native-editor/android/app/build/outputs/apk/debug/app-debug.apk"' \
+  --form "name=Gutenberg-$SAUCE_FILENAME.apk" \
+  --form 'description="Gutenberg"'
