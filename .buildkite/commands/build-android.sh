8bb31efe028d94932db1bc6714d53fc46d80707c
Gio Lodi <gio@mokacoding.com>
2023-09-13 14:03:01 +1000

    Add `--no-use` flag

--- .buildkite/commands/build-android.sh
+++ .buildkite/commands/build-android.sh
@@ -8,7 +8,7 @@ apt-get update
 apt-get install -y curl
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use
 nvm install "$(cat .nvmrc)"
 nvm install "$(cat jetpack/.nvmrc)"
 nvm use
