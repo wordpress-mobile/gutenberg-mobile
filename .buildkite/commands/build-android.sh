2b7371635a1e8751bbe20def6ec82dc250915015
Gio Lodi <gio@mokacoding.com>
2023-09-13 13:24:43 +1000

    More NVM setup

--- .buildkite/commands/build-android.sh
+++ .buildkite/commands/build-android.sh
@@ -7,6 +7,8 @@ echo '--- :node: Setup Node environment'
 apt-get update
 apt-get install -y curl
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install && nvm use
 
 echo '--- :npm: Install Node dependencies'
