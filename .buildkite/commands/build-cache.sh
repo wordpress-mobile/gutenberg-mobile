ac3c5d9560372de9ef3bd918779a23333e6a6898
Gio Lodi <gio@mokacoding.com>
2023-09-29 22:11:41 -0700

    Download build cache — Will it be worth the speed bump?

--- .buildkite/commands/build-cache.sh
+++ .buildkite/commands/build-cache.sh
@@ -4,6 +4,6 @@ echo '--- :node: Setup Node depenendencies'
 npm ci --unsafe-perm --prefer-offline --no-audit --no-progress
 
 echo "--- :arrow_up: Upload npm cache"
-NPM_CACHE_ARCHIVE=npm_home.tar.gz
-tar -czvf "$NPM_CACHE_ARCHIVE" -C "$HOME" .npm
+NPM_CACHE_ARCHIVE="npm_home.tar.gz"
+tar -czf "$NPM_CACHE_ARCHIVE" -C "$HOME" .npm
 buildkite-agent artifact upload "$NPM_CACHE_ARCHIVE"
