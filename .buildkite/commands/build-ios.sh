c0480c70ba81db8c7daca920e8fcc508b17e554e
Gerardo <gerardo.pacheco@automattic.com>
2023-11-02 17:28:57 +0100

    Update build-ios script to remove SauceLabs usage

--- .buildkite/commands/build-ios.sh
+++ .buildkite/commands/build-ios.sh
@@ -5,8 +5,6 @@ npm ci --unsafe-perm --prefer-offline --no-audit --no-progress
 
 echo '--- :ios: Set env var for iOS E2E testing'
 set -x
-export TEST_RN_PLATFORM=ios
-export TEST_ENV=sauce
 CONFIG_FILE="$(pwd)/gutenberg/packages/react-native-editor/__device-tests__/helpers/device-config.json"
 # Uses the local deviceName since SauceLabs uses a different one.
 IOS_DEVICE_NAME=$(jq -r '.ios.local.deviceName' "$CONFIG_FILE")
@@ -22,17 +20,23 @@ npm run core test:e2e:build-app:ios
 echo '--- :react: Build iOS bundle for E2E testing'
 npm run test:e2e:bundle:ios
 
-echo '--- :compression: Prepare artifact for SauceLabs upload'
+echo "--- :react: Prepare tests setup"
+npm run core test:e2e:setup
+
+echo '--- :react: Build WDA for E2E testing'
+npm run core test:e2e:build-wda
+
+echo '--- :compression: Prepare artifacts'
 WORK_DIR=$(pwd) \
   && pushd ./gutenberg/packages/react-native-editor/ios/build/GutenbergDemo/Build/Products/Release-iphonesimulator \
   && zip -r "$WORK_DIR/gutenberg/packages/react-native-editor/ios/GutenbergDemo.app.zip" GutenbergDemo.app \
   && popd
 
-echo '--- :saucelabs: Upload app artifact to SauceLabs'
-SAUCE_FILENAME=${BUILDKITE_BRANCH//[\/]/-}
-curl -u "$SAUCE_USERNAME:$SAUCE_ACCESS_KEY" \
-  --location \
-  --request POST 'https://api.us-west-1.saucelabs.com/v1/storage/upload' \
-  --form 'payload=@"./gutenberg/packages/react-native-editor/ios/GutenbergDemo.app.zip"' \
-  --form "name=Gutenberg-$SAUCE_FILENAME.app.zip" \
-  --form 'description="Gutenberg"'
+WORK_DIR=$(pwd) \
+  && pushd ./gutenberg/packages/react-native-editor/ios/build/WDA \
+  && zip -r "$WORK_DIR/gutenberg/packages/react-native-editor/ios/WDA.zip" ./* \
+  && popd
+
+echo "--- :arrow_up: Upload Build Products"
+upload_artifact "./gutenberg/packages/react-native-editor/ios/GutenbergDemo.app.zip"
+upload_artifact "./gutenberg/packages/react-native-editor/ios/WDA.zip"
