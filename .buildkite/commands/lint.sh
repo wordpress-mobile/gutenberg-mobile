62d5202fe0ee8c916fde44903c9cc510d7e03ba0
Carlos Garcia <fluiddot@gmail.com>
2024-01-04 18:14:33 +0100

    [TEST] Print available memory

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -eu
 
+node -e 'console.log("Heap size limit:",v8.getHeapStatistics().heap_size_limit/(1024*1024))'
+
+total_memory=$(free -m | awk '/^Mem:/{print $2}')
+available_memory=$(free -m | awk '/^Mem:/{print $7}')
+
+echo "Total Memory: ${total_memory}MB"
+echo "Available Memory: ${available_memory}MB"
+
 echo "--- :npm: Install Node dependencies"
 npm ci --unsafe-perm --prefer-offline --no-audit --no-progress
 
