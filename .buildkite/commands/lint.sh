8d5ecdd63cc6305131dc1adad054e811c58f667a
Gerardo <gerardo.pacheco@automattic.com>
2024-06-05 17:39:26 +0200

    Add check cache for lint step

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -2,9 +2,11 @@
 
 # Restore the caches, if any
 .buildkite/commands/install-node-dependencies.sh --restore-only
-# Set up the gutenberg-mobile dependencies without building the i18n cache (--ignore-scripts)
+# Set up the gutenberg-mobile dependencies without scripts (--ignore-scripts)
 echo "--- :npm: Install Node dependencies"
 npm ci --prefer-offline --no-progress --no-audit --ignore-scripts
+# Set up building the i18n cache
+npm run i18n:check-cache
 # Set up the gutenberg submodule dependencies
 npm ci --prefer-offline --no-progress --no-audit --prefix gutenberg --ignore-scripts
 
