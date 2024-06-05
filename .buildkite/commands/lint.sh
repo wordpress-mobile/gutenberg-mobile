a22a24cd271c79b93ed9389b72170ef3d51cf879
Gerardo <gerardo.pacheco@automattic.com>
2024-06-05 17:32:34 +0200

    Reduce npm installation for linting

--- .buildkite/commands/lint.sh
+++ .buildkite/commands/lint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -eu
 
-.buildkite/commands/install-node-dependencies.sh
+# Restore the caches, if any
+.buildkite/commands/install-node-dependencies.sh --restore-only
+# Set up the gutenberg-mobile dependencies without building the i18n cache (--ignore-scripts)
+echo "--- :npm: Install Node dependencies"
+npm ci --prefer-offline --no-progress --no-audit --ignore-scripts
+# Set up the gutenberg submodule dependencies
+npm ci --prefer-offline --no-progress --no-audit --prefix gutenberg --ignore-scripts
 
 echo "--- :node: Lint"
 CHECK_CORRECTNESS=true CHECK_TESTS=false ./bin/ci-checks-js.sh
