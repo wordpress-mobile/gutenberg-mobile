349dcf87a57d437bf0e61f2e6a3b4b79b17e07ae
Gerardo <gerardo.pacheco@automattic.com>
2024-06-05 19:25:38 +0200

    Test check if the files folder from npm exists

--- .buildkite/commands/install-node-dependencies.sh
+++ .buildkite/commands/install-node-dependencies.sh
@@ -22,6 +22,14 @@ echo "--- :npm: Restore cache if present"
 restore_cache "$CACHEKEY"
 restore_cache "$PNPM_CACHEKEY"
 
+# Check if /files/ folder exists after restoring cache
+if [ -d "./files" ]; then
+  echo "The folder /files/ exists."
+else
+  echo "The folder /files/ does not exist."
+fi
+
+
 if [[ "${RESTORE_ONLY}" ==  'true' ]]; then
   echo 'Exiting after restoring caches as per --restore-only call parameter.'
   exit 0
