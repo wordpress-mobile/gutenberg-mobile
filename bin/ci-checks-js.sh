dd83a381bf017dca4e19447f82a756fa93d0a4b6
Gerardo <gerardo.pacheco@automattic.com>
2024-06-05 17:22:48 +0200

    Update lint command

--- bin/ci-checks-js.sh
+++ bin/ci-checks-js.sh
@@ -38,12 +38,6 @@ if [ "$CHECK_CORRECTNESS" = true ] ; then
   checkDiff
 
   echo "--- :eslint: Lint"
-  # Need to build gutenberg packages before linting so that eslint-plugin-import can resolve those.
-  # See https://github.com/WordPress/gutenberg/pull/22088 for more information.
-  cd gutenberg
-  npm run build:packages || pFail
-  cd ..
-
   npm run lint || pFail
 fi
 
